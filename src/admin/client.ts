d5ff803d7d57f928b3d15148bf2d0b15f4998615
Michael Grosner <michaelgrosner@gmail.com>
2015-02-01 17:15:12 -0500

    Got rid of Client module

--- src/admin/client.ts
+++ src/admin/client.ts
@@ -11,130 +11,128 @@ import Trades = require("./trades");
 import Messaging = require("../common/messaging");
 import Shared = require("./shared_directives");
 
-module Client {
-    interface MainWindowScope extends ng.IScope {
-        env : string;
-        connected : boolean;
-        order : DisplayOrder;
+interface MainWindowScope extends ng.IScope {
+    env : string;
+    connected : boolean;
+    order : DisplayOrder;
 
-        exchanges : Exchange.DisplayExchangeInformation[];
+    exchanges : Exchange.DisplayExchangeInformation[];
+}
+
+class DisplayPair {
+    public displayName : string;
+    constructor(public pair : Models.CurrencyPair) {
+        this.displayName = Models.Currency[pair.base] + "/" + Models.Currency[pair.quote];
     }
+}
 
-    class DisplayPair {
-        public displayName : string;
-        constructor(public pair : Models.CurrencyPair) {
-            this.displayName = Models.Currency[pair.base] + "/" + Models.Currency[pair.quote];
-        }
-    }
+class DisplayOrder {
+    exchange : string;
+    side : string;
+    price : number;
+    quantity : number;
+    timeInForce : string;
+    orderType : string;
+    pair : DisplayPair;
 
-    class DisplayOrder {
-        exchange : string;
-        side : string;
-        price : number;
-        quantity : number;
-        timeInForce : string;
-        orderType : string;
-        pair : DisplayPair;
+    availableExchanges : string[];
+    availableSides : string[];
+    availableTifs : string[];
+    availableOrderTypes : string[];
+    availablePairs : DisplayPair[] = [];
 
-        availableExchanges : string[];
-        availableSides : string[];
-        availableTifs : string[];
-        availableOrderTypes : string[];
-        availablePairs : DisplayPair[] = [];
-
-        private static getNames<T>(enumObject : T) {
-            var names : string[] = [];
-            for (var mem in enumObject) {
-                if (!enumObject.hasOwnProperty(mem)) continue;
-                if (parseInt(mem, 10) >= 0) {
-                  names.push(enumObject[mem]);
-                }
+    private static getNames<T>(enumObject : T) {
+        var names : string[] = [];
+        for (var mem in enumObject) {
+            if (!enumObject.hasOwnProperty(mem)) continue;
+            if (parseInt(mem, 10) >= 0) {
+              names.push(enumObject[mem]);
             }
-            return names;
         }
-
-        private _fire : Messaging.IFire<Models.OrderRequestFromUI>;
-        constructor(socket : SocketIOClientStatic, private _log : ng.ILogService) {
-            this.availableExchanges = DisplayOrder.getNames(Models.Exchange);
-            this.availableSides = DisplayOrder.getNames(Models.Side);
-            this.availableTifs = DisplayOrder.getNames(Models.TimeInForce);
-            this.availableOrderTypes = DisplayOrder.getNames(Models.OrderType);
-
-            this._fire = new Messaging.Fire<Models.OrderRequestFromUI>(Messaging.Topics.SubmitNewOrder, socket, _log.info);
-        }
-
-        public submit = () => {
-            var msg = new Models.OrderRequestFromUI(this.exchange,
-                this.side, this.price, this.quantity, this.timeInForce, this.orderType, this.pair.pair);
-            this._log.info("submitting order", msg);
-            this._fire.fire(msg);
-        };
-
-        public addNewPair = (p : Models.CurrencyPair) => {
-            for (var i = 0; i < this.availablePairs.length; i++) {
-                if (Models.currencyPairEqual(this.availablePairs[i].pair, p))
-                    return;
-            }
-
-            this.availablePairs.push(new DisplayPair(p));
-        };
+        return names;
     }
 
-    var uiCtrl = ($scope : MainWindowScope, $timeout : ng.ITimeoutService, $log : ng.ILogService,
-                  socket : SocketIOClientStatic, productListings : Shared.ProductListingRegistrar) => {
-        $scope.connected = false;
-        $scope.exchanges = [];
-        $scope.order = new DisplayOrder(socket, $log);
+    private _fire : Messaging.IFire<Models.OrderRequestFromUI>;
+    constructor(socket : SocketIOClientStatic, private _log : ng.ILogService) {
+        this.availableExchanges = DisplayOrder.getNames(Models.Exchange);
+        this.availableSides = DisplayOrder.getNames(Models.Side);
+        this.availableTifs = DisplayOrder.getNames(Models.TimeInForce);
+        this.availableOrderTypes = DisplayOrder.getNames(Models.OrderType);
 
-        var onConnect = () => {
-            $scope.connected = true;
-            $scope.exchanges = [];
-        };
+        this._fire = new Messaging.Fire<Models.OrderRequestFromUI>(Messaging.Topics.SubmitNewOrder, socket, _log.info);
+    }
 
-        var onAdvert = (pa : Models.ProductAdvertisement) => {
-            $scope.order.addNewPair(pa.pair);
-
-            var getExchInfo = () : Exchange.DisplayExchangeInformation => {
-                for (var i = 0; i < $scope.exchanges.length; i++) {
-                    if ($scope.exchanges[i].exchange === pa.exchange)
-                        return $scope.exchanges[i];
-                }
-
-                $log.info("adding new exchange", Models.Exchange[pa.exchange]);
-                var exch = new Exchange.DisplayExchangeInformation($log, pa.exchange, socket);
-                $scope.exchanges.push(exch);
-                return exch;
-            };
-
-            getExchInfo().getOrAddDisplayPair(pa.pair);
-        };
-
-        var onDisconnect = () => {
-            $scope.connected = false;
-            $scope.exchanges.forEach(x => x.dispose());
-            $scope.exchanges = [];
-        };
-
-        productListings.registerOnConnect(onConnect);
-        productListings.registerOnAdvert(onAdvert);
-        productListings.registerOnDisconnect(onDisconnect);
-
-        var refresh_timer = () => {
-            $timeout(refresh_timer, 250);
-        };
-        $timeout(refresh_timer, 250);
-
-        $log.info("started client");
+    public submit = () => {
+        var msg = new Models.OrderRequestFromUI(this.exchange,
+            this.side, this.price, this.quantity, this.timeInForce, this.orderType, this.pair.pair);
+        this._log.info("submitting order", msg);
+        this._fire.fire(msg);
     };
 
-    var requires = ['ui.bootstrap',
-                    'ngGrid',
-                    'orderListDirective',
-                    'tradeListDirective',
-                    'marketQuotingDirective',
-                    'marketTradeDirective',
-                    'sharedDirectives'];
+    public addNewPair = (p : Models.CurrencyPair) => {
+        for (var i = 0; i < this.availablePairs.length; i++) {
+            if (Models.currencyPairEqual(this.availablePairs[i].pair, p))
+                return;
+        }
 
-    angular.module('projectApp', requires)
-           .controller('uiCtrl', uiCtrl)
+        this.availablePairs.push(new DisplayPair(p));
+    };
 }
+
+var uiCtrl = ($scope : MainWindowScope, $timeout : ng.ITimeoutService, $log : ng.ILogService,
+              socket : SocketIOClientStatic, productListings : Shared.ProductListingRegistrar) => {
+    $scope.connected = false;
+    $scope.exchanges = [];
+    $scope.order = new DisplayOrder(socket, $log);
+
+    var onConnect = () => {
+        $scope.connected = true;
+        $scope.exchanges = [];
+    };
+
+    var onAdvert = (pa : Models.ProductAdvertisement) => {
+        $scope.order.addNewPair(pa.pair);
+
+        var getExchInfo = () : Exchange.DisplayExchangeInformation => {
+            for (var i = 0; i < $scope.exchanges.length; i++) {
+                if ($scope.exchanges[i].exchange === pa.exchange)
+                    return $scope.exchanges[i];
+            }
+
+            $log.info("adding new exchange", Models.Exchange[pa.exchange]);
+            var exch = new Exchange.DisplayExchangeInformation($log, pa.exchange, socket);
+            $scope.exchanges.push(exch);
+            return exch;
+        };
+
+        getExchInfo().getOrAddDisplayPair(pa.pair);
+    };
+
+    var onDisconnect = () => {
+        $scope.connected = false;
+        $scope.exchanges.forEach(x => x.dispose());
+        $scope.exchanges = [];
+    };
+
+    productListings.registerOnConnect(onConnect);
+    productListings.registerOnAdvert(onAdvert);
+    productListings.registerOnDisconnect(onDisconnect);
+
+    var refresh_timer = () => {
+        $timeout(refresh_timer, 250);
+    };
+    $timeout(refresh_timer, 250);
+
+    $log.info("started client");
+};
+
+var requires = ['ui.bootstrap',
+                'ngGrid',
+                'orderListDirective',
+                'tradeListDirective',
+                'marketQuotingDirective',
+                'marketTradeDirective',
+                'sharedDirectives'];
+
+angular.module('projectApp', requires)
+       .controller('uiCtrl', uiCtrl);
