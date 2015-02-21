40818aff71ed0f1dabbf1bb3452da0cb972e4033
Michael Grosner <michaelgrosner@gmail.com>
2015-02-21 13:40:21 -0500

    Dont connect until after everythings been loaded

--- src/service/main.ts
+++ src/service/main.ts
@@ -30,86 +30,26 @@ import Winkdex = require("./winkdex");
 var mainLog = Utils.log("tribeca:main");
 var messagingLog = Utils.log("tribeca:messaging");
 
-var app = express();
-var http = (<any>require('http')).Server(app);
-var io = require('socket.io')(http);
-
-app.use(<any>compression());
-app.use(express.static(path.join(__dirname, "admin")));
-http.listen(3000, () => mainLog('Listening to admins on *:3000...'));
-
 var env = process.env.TRIBECA_MODE;
 var config = new Config.ConfigProvider(env);
 
-var orderCache = new Broker.OrderStateCache();
+var pair = new Models.CurrencyPair(Models.Currency.BTC, Models.Currency.USD);
 
-var getExch = () : Interfaces.CombinedGateway => {
+var getExchange = () : Models.Exchange => {
     var ex = process.env.EXCHANGE.toLowerCase();
     switch (ex) {
-        case "hitbtc": return <Interfaces.CombinedGateway>(new HitBtc.HitBtc(config));
-        case "okcoin": return <Interfaces.CombinedGateway>(new OkCoin.OkCoin(config));
-        case "coinbase": return <Interfaces.CombinedGateway>(new Coinbase.Coinbase(config, orderCache));
-        case "null": return <Interfaces.CombinedGateway>(new NullGw.NullGateway());
+        case "hitbtc": return Models.Exchange.HitBtc;
+        case "okcoin": return Models.Exchange.OkCoin;
+        case "coinbase": return Models.Exchange.Coinbase;
+        case "null": return Models.Exchange.Null;
         default: throw new Error("unknown configuration env variable EXCHANGE " + ex);
     }
 };
-
-var gateway = getExch();
-var exchange = gateway.base.exchange();
-
-var pair = new Models.CurrencyPair(Models.Currency.BTC, Models.Currency.USD);
-var advert = new Models.ProductAdvertisement(gateway.base.exchange(), pair, Config.Environment[config.environment()]);
-new Messaging.Publisher<Models.ProductAdvertisement>(Messaging.Topics.ProductAdvertisement, io, () => [advert]).publish(advert);
+var exchange = getExchange();
 
 var getEngineTopic = (topic : string) : string => {
     return Messaging.ExchangePairMessaging.wrapExchangePairTopic(exchange, pair, topic);
 };
-
-var getEnginePublisher = <T>(topic : string) : Messaging.IPublish<T> => {
-    var wrappedTopic = getEngineTopic(topic);
-    var socketIoPublisher = new Messaging.Publisher<T>(wrappedTopic, io, null, Utils.log("tribeca:messaging"));
-    return new Web.HttpPublisher<T>(topic, socketIoPublisher, app);
-};
-
-var quotePublisher = getEnginePublisher<Models.TwoSidedQuote>(Messaging.Topics.Quote);
-var fvPublisher = getEnginePublisher(Messaging.Topics.FairValue);
-var marketDataPublisher = getEnginePublisher(Messaging.Topics.MarketData);
-var orderStatusPublisher = getEnginePublisher(Messaging.Topics.OrderStatusReports);
-var tradePublisher = getEnginePublisher(Messaging.Topics.Trades);
-var safetySettingsPublisher = getEnginePublisher(Messaging.Topics.SafetySettings);
-var activePublisher = getEnginePublisher(Messaging.Topics.ActiveChange);
-var quotingParametersPublisher = getEnginePublisher(Messaging.Topics.QuotingParametersChange);
-var marketTradePublisher = getEnginePublisher(Messaging.Topics.MarketTrade);
-var messagesPublisher = getEnginePublisher(Messaging.Topics.Message);
-var externalValuationPublisher = getEnginePublisher(Messaging.Topics.ExternalValuation);
-
-var messages = new Broker.MessagesPubisher(messagesPublisher);
-messages.publish("start up");
-
-var getHttpPublisher = <T>(topic : string) : Web.StandaloneHttpPublisher<T> => {
-    return new Web.StandaloneHttpPublisher<T>(topic, app);
-};
-
-var tradeHttpPublisher = getHttpPublisher("trades");
-var latencyHttpPublisher = getHttpPublisher("latency");
-
-var getExchangePublisher = <T>(topic : string) => {
-    var wrappedTopic = Messaging.ExchangePairMessaging.wrapExchangeTopic(gateway.base.exchange(), topic);
-    return new Messaging.Publisher<T>(wrappedTopic, io, null, messagingLog);
-};
-
-var positionPublisher = getExchangePublisher(Messaging.Topics.Position);
-var connectivity = getExchangePublisher(Messaging.Topics.ExchangeConnectivity);
-
-var getReceiver = <T>(topic : string) => {
-    return new Messaging.Receiver<T>(getEngineTopic(topic), io, messagingLog);
-};
-
-var safetySettingsReceiver = getReceiver(Messaging.Topics.SafetySettings);
-var activeReceiver = getReceiver(Messaging.Topics.ActiveChange);
-var quotingParametersReceiver = getReceiver(Messaging.Topics.QuotingParametersChange);
-var submitOrderReceiver = new Messaging.Receiver(Messaging.Topics.SubmitNewOrder, io, messagingLog);
-var cancelOrderReceiver = new Messaging.Receiver(Messaging.Topics.CancelOrder, io, messagingLog);
 
 var db = Persister.loadDb();
 var orderPersister = new Persister.OrderStatusPersister(db);
@@ -132,6 +72,77 @@ Q.all([
            initMktTrades : Models.ExchangePairMessage<Models.MarketTrade>[], 
            initSafety : Models.SafetySettings, 
            initParams : Models.QuotingParameters) => {
+
+    var app = express();
+    var http = (<any>require('http')).Server(app);
+    var io = require('socket.io')(http);
+
+    app.use(<any>compression());
+    app.use(express.static(path.join(__dirname, "admin")));
+    http.listen(3000, () => mainLog('Listening to admins on *:3000...'));
+
+    var advert = new Models.ProductAdvertisement(exchange, pair, Config.Environment[config.environment()]);
+    new Messaging.Publisher<Models.ProductAdvertisement>(Messaging.Topics.ProductAdvertisement, io, () => [advert]).publish(advert);
+
+    var getEnginePublisher = <T>(topic : string) : Messaging.IPublish<T> => {
+        var wrappedTopic = getEngineTopic(topic);
+        var socketIoPublisher = new Messaging.Publisher<T>(wrappedTopic, io, null, Utils.log("tribeca:messaging"));
+        return new Web.HttpPublisher<T>(topic, socketIoPublisher, app);
+    };
+
+    var quotePublisher = getEnginePublisher<Models.TwoSidedQuote>(Messaging.Topics.Quote);
+    var fvPublisher = getEnginePublisher(Messaging.Topics.FairValue);
+    var marketDataPublisher = getEnginePublisher(Messaging.Topics.MarketData);
+    var orderStatusPublisher = getEnginePublisher(Messaging.Topics.OrderStatusReports);
+    var tradePublisher = getEnginePublisher(Messaging.Topics.Trades);
+    var safetySettingsPublisher = getEnginePublisher(Messaging.Topics.SafetySettings);
+    var activePublisher = getEnginePublisher(Messaging.Topics.ActiveChange);
+    var quotingParametersPublisher = getEnginePublisher(Messaging.Topics.QuotingParametersChange);
+    var marketTradePublisher = getEnginePublisher(Messaging.Topics.MarketTrade);
+    var messagesPublisher = getEnginePublisher(Messaging.Topics.Message);
+    var externalValuationPublisher = getEnginePublisher(Messaging.Topics.ExternalValuation);
+
+    var messages = new Broker.MessagesPubisher(messagesPublisher);
+    messages.publish("start up");
+
+    var getHttpPublisher = <T>(topic : string) : Web.StandaloneHttpPublisher<T> => {
+        return new Web.StandaloneHttpPublisher<T>(topic, app);
+    };
+
+    var tradeHttpPublisher = getHttpPublisher("trades");
+    var latencyHttpPublisher = getHttpPublisher("latency");
+
+    var getExchangePublisher = <T>(topic : string) => {
+        var wrappedTopic = Messaging.ExchangePairMessaging.wrapExchangeTopic(exchange, topic);
+        return new Messaging.Publisher<T>(wrappedTopic, io, null, messagingLog);
+    };
+
+    var positionPublisher = getExchangePublisher(Messaging.Topics.Position);
+    var connectivity = getExchangePublisher(Messaging.Topics.ExchangeConnectivity);
+
+    var getReceiver = <T>(topic : string) => {
+        return new Messaging.Receiver<T>(getEngineTopic(topic), io, messagingLog);
+    };
+
+    var safetySettingsReceiver = getReceiver(Messaging.Topics.SafetySettings);
+    var activeReceiver = getReceiver(Messaging.Topics.ActiveChange);
+    var quotingParametersReceiver = getReceiver(Messaging.Topics.QuotingParametersChange);
+    var submitOrderReceiver = new Messaging.Receiver(Messaging.Topics.SubmitNewOrder, io, messagingLog);
+    var cancelOrderReceiver = new Messaging.Receiver(Messaging.Topics.CancelOrder, io, messagingLog);
+
+    var orderCache = new Broker.OrderStateCache();
+
+    var getExch = () : Interfaces.CombinedGateway => {
+        switch (exchange) {
+            case Models.Exchange.HitBtc: return <Interfaces.CombinedGateway>(new HitBtc.HitBtc(config));
+            case Models.Exchange.OkCoin: return <Interfaces.CombinedGateway>(new OkCoin.OkCoin(config));
+            case Models.Exchange.Coinbase: return <Interfaces.CombinedGateway>(new Coinbase.Coinbase(config, orderCache));
+            case Models.Exchange.Null: return <Interfaces.CombinedGateway>(new NullGw.NullGateway());
+            default: throw new Error("no gateway provided for exchange " + exchange);
+        }
+    };
+
+    var gateway = getExch();
 
     var broker = new Broker.ExchangeBroker(pair, gateway.md, gateway.base, gateway.oe, gateway.pg, connectivity);
     var orderBroker = new Broker.OrderBroker(broker, gateway.oe, orderPersister, tradesPersister, orderStatusPublisher,
