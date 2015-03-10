7ffb6a2f67ba2700b5a14f30882af028c80b3da8
Michael Grosner <michaelgrosner@gmail.com>
2015-03-10 18:07:20 -0400

    Grunt for prod compile

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,4 +67,5 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
 
     grunt.registerTask("default", ["ts", "copy", "watch"]);
+    grunt.registerTask("compile", ["ts", "copy"])
 };
