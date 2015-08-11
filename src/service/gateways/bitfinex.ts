d6b55657ba10a706df38360edad9c763ba8a2548
Michael Grosner <michaelgrosner@gmail.com>
2015-08-10 20:08:28 -0400

    Added stub gateway class for bitfinex

---
+++ src/service/gateways/bitfinex.ts
@@ -0,0 +1,197 @@
+/// <reference path="../../../typings/tsd.d.ts" />
+/// <reference path="../utils.ts" />
+/// <reference path="../../common/models.ts" />
+/// <reference path="nullgw.ts" />
+
+import ws = require('ws');
+import Q = require("q");
+import crypto = require("crypto");
+import request = require("request");
+import url = require("url");
+import querystring = require("querystring");
+import Config = require("../config");
+import NullGateway = require("./nullgw");
+import Models = require("../../common/models");
+import Utils = require("../utils");
+import util = require("util");
+import Interfaces = require("../interfaces");
+import moment = require("moment");
+import _ = require("lodash");
+var shortId = require("shortid");
+
+interface BitfinexMarketTrade {
+	tid: number;
+	timestamp: number;
+	price: string;
+	amount: string;
+	exchange: string;
+	type: string;
+}
+
+class BitfinexMarketDataGateway implements Interfaces.IMarketDataGateway {
+    ConnectChanged = new Utils.Evt<Models.ConnectivityStatus>();
+
+    MarketTrade = new Utils.Evt<Models.GatewayMarketTrade>();
+    private onTrades = (trades : Models.Timestamped<BitfinexMarketTrade[]>) => {
+        _.forEach(trades.data, trade => {
+            //this.MarketTrade.trigger(mt);
+        });
+    };
+
+    MarketData = new Utils.Evt<Models.Market>();
+
+    private _log : Utils.Logger = Utils.log("tribeca:gateway:BitfinexMD");
+    constructor(private _symbolProvider: BitfinexSymbolProvider) {
+        
+    }
+}
+
+class BitfinexOrderEntryGateway implements Interfaces.IOrderEntryGateway {
+    OrderUpdate = new Utils.Evt<Models.OrderStatusReport>();
+    ConnectChanged = new Utils.Evt<Models.ConnectivityStatus>();
+
+    generateClientOrderId = () => shortId.generate();
+
+    public cancelsByClientOrderId = false;
+    
+    sendOrder = (order : Models.BrokeredOrder) : Models.OrderGatewayActionReport => {
+        return new Models.OrderGatewayActionReport(Utils.date());
+    };
+    
+    cancelOrder = (cancel : Models.BrokeredCancel) : Models.OrderGatewayActionReport => {
+        return new Models.OrderGatewayActionReport(Utils.date());
+    };
+
+    replaceOrder = (replace : Models.BrokeredReplace) : Models.OrderGatewayActionReport => {
+        this.cancelOrder(new Models.BrokeredCancel(replace.origOrderId, replace.orderId, replace.side, replace.exchangeId));
+        return this.sendOrder(replace);
+    };
+    
+    private _log : Utils.Logger = Utils.log("tribeca:gateway:BitfinexOE");
+    constructor(private _symbolProvider: BitfinexSymbolProvider) {
+    }
+}
+
+class BitfinexHttp {
+    post = <T>(actionUrl: string, msg : any) : Q.Promise<Models.Timestamped<T>> => {
+        var d = Q.defer<Models.Timestamped<T>>();
+
+        request({
+            url: url.resolve(this._baseUrl, actionUrl),
+            body: querystring.stringify({}),
+            headers: {"Content-Type": "application/x-www-form-urlencoded"},
+            method: "POST"
+        }, (err, resp, body) => {
+            if (err) d.reject(err);
+            else {
+                try {
+                    var t = Utils.date();
+                    var data = JSON.parse(body);
+                    d.resolve(new Models.Timestamped(data, t));
+                }
+                catch (e) {
+                    this._log("url: %s, err: %o, body: %o", actionUrl, err, body);
+                    d.reject(e);
+                }
+            }
+        });
+        
+        return d.promise;
+    };
+
+    private _log : Utils.Logger = Utils.log("tribeca:gateway:BitfinexHTTP");
+    private _baseUrl : string;
+    constructor(config : Config.IConfigProvider) {
+        this._baseUrl = config.GetString("BitfinexHttpUrl")
+    }
+}
+
+class BitfinexPositionGateway implements Interfaces.IPositionGateway {
+    PositionUpdate = new Utils.Evt<Models.CurrencyPosition>();
+
+    private static convertCurrency(name : string) : Models.Currency {
+        switch (name.toLowerCase()) {
+            case "usd": return Models.Currency.USD;
+            case "ltc": return Models.Currency.LTC;
+            case "btc": return Models.Currency.BTC;
+            default: throw new Error("Unsupported currency " + name);
+        }
+    }
+
+    private _log : Utils.Logger = Utils.log("tribeca:gateway:BitfinexPG");
+    constructor() {
+    }
+}
+
+class BitfinexBaseGateway implements Interfaces.IExchangeDetailsGateway {
+    public get hasSelfTradePrevention() {
+        return false;
+    }
+
+    name() : string {
+        return "Bitfinex";
+    }
+
+    makeFee() : number {
+        return 0.001;
+    }
+
+    takeFee() : number {
+        return 0.002;
+    }
+
+    exchange() : Models.Exchange {
+        return Models.Exchange.Bitfinex;
+    }
+    
+    private static AllPairs = [
+        new Models.CurrencyPair(Models.Currency.BTC, Models.Currency.USD),
+        //new Models.CurrencyPair(Models.Currency.LTC, Models.Currency.USD),
+    ];
+    public get supportedCurrencyPairs() {
+        return BitfinexBaseGateway.AllPairs;
+    }
+}
+
+function GetCurrencyEnum(c: string) : Models.Currency {
+    switch (name.toLowerCase()) {
+        case "usd": return Models.Currency.USD;
+        case "ltc": return Models.Currency.LTC;
+        case "btc": return Models.Currency.BTC;
+        default: throw new Error("Unsupported currency " + name);
+    }
+}
+
+function GetCurrencySymbol(c: Models.Currency) : string {
+    switch (c) {
+        case Models.Currency.USD: return "usd";
+        case Models.Currency.LTC: return "ltc";
+        case Models.Currency.BTC: return "btc";
+        default: throw new Error("Unsupported currency " + Models.Currency[c]);
+    }
+}
+
+class BitfinexSymbolProvider {
+    public symbol : string;
+    
+    constructor(pair: Models.CurrencyPair) {
+        this.symbol = GetCurrencySymbol(pair.base) + GetCurrencySymbol(pair.quote);
+    }
+}
+
+export class Bitfinex extends Interfaces.CombinedGateway {
+    constructor(config : Config.IConfigProvider, pair: Models.CurrencyPair) {
+        var symbol = new BitfinexSymbolProvider(pair);
+        var http = new BitfinexHttp(config);
+
+        var orderGateway = config.GetString("BitfinexOrderDestination") == "Bitfinex"
+            ? <Interfaces.IOrderEntryGateway>new BitfinexOrderEntryGateway(symbol)
+            : new NullGateway.NullOrderGateway();
+
+        super(
+            new BitfinexMarketDataGateway(symbol),
+            orderGateway,
+            new BitfinexPositionGateway(),
+            new BitfinexBaseGateway());
+        }
+}
