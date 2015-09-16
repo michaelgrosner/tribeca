b0a676545771caa7deeea045cdb9fbf554ce9a9a
Michael Grosner <michaelgrosner@gmail.com>
2015-09-15 22:12:37 -0400

    Refactor out the quoting styles from the QuotingEngine, on the road to make it easier for users to add new styles

---
+++ src/service/quoting-styles/style-registry.ts
@@ -0,0 +1,31 @@
+/// <reference path="../../common/models.ts" />
+/// <reference path="../../../typings/tsd.d.ts" />
+
+import StyleHelpers = require("./helpers");
+import Models = require("../../common/models");
+import _ = require("lodash");
+
+export class QuotingStyleRegistry {
+	private _mapping : StyleHelpers.QuoteStyle[];
+	
+	constructor(private _modules: StyleHelpers.QuoteStyle[]) {}
+	
+	private static NullQuoteGenerator : StyleHelpers.QuoteStyle = new NullQuoteGenerator();
+	
+	public Get = (mode : Models.QuotingMode) : StyleHelpers.QuoteStyle => {
+		var mod = this._modules[mode];
+		
+		if (typeof mod === "undefined")
+			return QuotingStyleRegistry.NullQuoteGenerator;
+			
+		return mod;
+	};
+}
+
+class NullQuoteGenerator implements StyleHelpers.QuoteStyle {
+	Mode = null;
+    
+    GenerateQuote = (market: Models.Market, fv: Models.FairValue, params: Models.QuotingParameters) : StyleHelpers.GeneratedQuote => {
+		return null;
+	};
+}
