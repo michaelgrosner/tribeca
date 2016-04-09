7e1ce904f0194151174bbfd08cdc2b581d8999c6
ctubio <analpaper@gmail.com>
2016-04-09 15:39:52 +0200

    Added displays.

--- src/admin/position.ts
+++ src/admin/position.ts
@@ -20,6 +20,7 @@ interface PositionScope extends ng.IScope {
     baseHeldPosition : number;
     quoteHeldPosition : number;
     value : number;
+    valueFiat : number;
 }
 
 var PositionController = ($scope : PositionScope, $log : ng.ILogService, subscriberFactory : Shared.SubscriberFactory) => {
@@ -41,6 +42,7 @@ var PositionController = ($scope : PositionScope, $log : ng.ILogService, subscri
         $scope.baseHeldPosition = position.baseHeldAmount;
         $scope.quoteHeldPosition = position.quoteHeldAmount;
         $scope.value = position.value;
+        $scope.valueFiat = position.valueFiat;
     };
 
     var positionSubscriber = subscriberFactory.getSubscriber($scope, Messaging.Topics.Position)
