55f05022eafe583c8fda0939e119ceb0fa909bd8
ctubio <analpaper@gmail.com>
2016-04-10 14:05:01 +0200

    Added width of qBid/qAsk.

--- src/admin/market-quoting.ts
+++ src/admin/market-quoting.ts
@@ -87,8 +87,11 @@ var MarketQuotingController = ($scope: MarketQuotingScope,
                 $scope.levels[i] = new Level();
             $scope.levels[i].bidPrice = update.bids[i].price;
             $scope.levels[i].bidSize = update.bids[i].size;
-            $scope.levels[i].diffWidth = (i==0)
-              ? $scope.levels[i].askPrice - $scope.levels[i].bidPrice : 0;
+            $scope.levels[i].diffWidth = i==0
+              ? $scope.levels[i].askPrice - $scope.levels[i].bidPrice : (
+                (i==1 && $scope.qBidPx && $scope.qAskPx)
+                  ? $scope.qBidPx - $scope.qAskPx : 0
+              );
         }
 
 
