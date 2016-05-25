39ed4e799ae2c622bb555f0c5c17d8d805fc8fe6
ctubio <analpaper@gmail.com>
2016-05-25 02:20:08 +0200

    Exception while downloading Coinbase positions (Unsupported currency ETH).

--- src/common/models.ts
+++ src/common/models.ts
@@ -58,7 +58,7 @@ export class MarketTrade implements ITimestamped {
 }
 
 export enum GatewayType { MarketData, OrderEntry, Position }
-export enum Currency { USD, BTC, LTC, EUR, GBP, CNY }
+export enum Currency { USD, BTC, LTC, EUR, GBP, CNY , ETH }
 export enum ConnectivityStatus { Connected, Disconnected }
 export enum Exchange { Null, HitBtc, OkCoin, AtlasAts, BtcChina, Coinbase, Bitfinex }
 export enum Side { Bid, Ask, Unknown }
