eaf2e561423bb5c7550a97ed28989c8465c4bd4b
ctubio <analpaper@gmail.com>
2016-07-24 04:57:33 +0200

    Fixed calcs.

--- src/service/persister.ts
+++ src/service/persister.ts
@@ -183,7 +183,7 @@ export class Persister<T extends Persistable> implements ILoadAll<T> {
               { price: side==Models.Side.Bid?{ $gt: width+price }:{ $lt: price-width } },
               { side: side==Models.Side.Bid?1:0 },
               { $where: "this.quantity - this.alloc > 0" }
-            ] }).limit(1).sort({ alloc: 1, price: side==Models.Side.Bid?-1:1 })
+            ] }).limit(1).sort({ alloc: 1, price: side==Models.Side.Bid?1:-1 })
             .toArray((err, arr) => {
                 if (err) {
                   this._log.info('prefind-err');
