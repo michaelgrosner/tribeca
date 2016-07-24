114d46355fe7744249f0485a73339ce34c59ddfb
ctubio <analpaper@gmail.com>
2016-07-24 15:54:05 +0200

    Fixed calcs.

--- src/admin/trades.ts
+++ src/admin/trades.ts
@@ -60,7 +60,7 @@ var TradesListController = ($scope : TradesScope, $log : ng.ILogService, subscri
         columnDefs: [
             {width: 75, field:'time', displayName:'t', cellFilter: 'momentShortDate',
                 sortingAlgorithm: (a: moment.Moment, b: moment.Moment) => a.diff(b),
-                sort: { direction: uiGridConstants.ASC, priority: 1} },
+                sort: { direction: uiGridConstants.ASC, priority: 2} },
             {width: 50, field:'price', displayName:'px', cellFilter: 'currency'},
             {width: 50, field:'quantity', displayName:'qty'},
             {width: 20, field:'side', displayName:'side', cellClass: (grid, row, col, rowRenderIndex, colRenderIndex) => {
@@ -77,7 +77,7 @@ var TradesListController = ($scope : TradesScope, $log : ng.ILogService, subscri
             {width: 60, field:'value', displayName:'val', cellFilter: 'currency:"$":3'},
             {width: 50, field:'alloc', displayName:'alloc'},
             {width: 55, field:'allocprice', displayName:'px', cellFilter: 'currency',
-                sort: { direction: uiGridConstants.DESC, priority: 2}}
+                sort: { direction: uiGridConstants.DESC, priority: 1}}
         ]
     };
 
