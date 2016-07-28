4cfa8f9dae9b6e764de27096cc2119cbebebe9d5
ctubio <analpaper@gmail.com>
2016-07-28 21:05:37 +0200

    Fixed calcs.

--- src/service/persister.ts
+++ src/service/persister.ts
@@ -169,7 +169,7 @@ export class Persister<T extends Persistable> implements ILoadAll<T> {
         this.collection.then(coll => {
             this._saver(report);
             if (this._dbName=="fv" || this._dbName=="md" || this._dbName=="tsv")
-              coll.deleteMany({ $where: "this.price > 0" }, err => {
+              coll.deleteMany({ time: { $exists:true } }, err => {
                   if (err)
                       this._log.error(err, "Unable to deleteMany", this._dbName, report);
               });
