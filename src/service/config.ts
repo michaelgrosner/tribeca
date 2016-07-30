8349dffc5264bf915e043b04acf8966d45731ef2
ctubio <c@rles-tub.io>
2016-07-30 19:30:46 +0200

    Fixed calcs.

--- src/service/config.ts
+++ src/service/config.ts
@@ -7,7 +7,7 @@ import fs = require("fs");
 export interface IConfigProvider {
     GetString(configKey: string): string;
     GetNumber(configKey: string): number;
-    
+
     inBacktestMode: boolean;
 }
 
@@ -17,7 +17,7 @@ export class ConfigProvider implements IConfigProvider {
 
     constructor() {
         this.inBacktestMode = (process.env["TRIBECA_BACKTEST_MODE"] || "false") === "true";
-        
+
         var configFile = process.env["TRIBECA_CONFIG_FILE"] || "tribeca.json";
         if (fs.existsSync(configFile)) {
             this._config = JSON.parse(fs.readFileSync(configFile, "utf-8"));
@@ -33,16 +33,16 @@ export class ConfigProvider implements IConfigProvider {
         ConfigProvider.Log.info("%s = %s", configKey, value);
         return value;
     };
-    
+
     private Fetch = (configKey: string): string => {
         if (process.env.hasOwnProperty(configKey))
             return process.env[configKey];
-        
+
         if (this._config.hasOwnProperty(configKey))
             return this._config[configKey];
 
         throw Error("Config does not have property " + configKey);
     };
-    
+
     inBacktestMode: boolean = false;
 }
