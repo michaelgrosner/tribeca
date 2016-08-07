4bde4b4b081efb55c9103a68ddc0db66f3788a9a
ctubio <c@rles-tub.io>
2016-08-07 19:22:19 +0200

    Fixed truncated collections.

--- src/service/persister.ts
+++ src/service/persister.ts
@@ -106,11 +106,6 @@ export class RepositoryPersister<T extends Persistable> implements ILoadLatest<T
     public persist = (report: T) => {
         this._saver(report);
         this.collection.then(coll => {
-            if (this._dbName != 'trades')
-              coll.deleteMany({ _id: { $exists:true } }, err => {
-                  if (err)
-                      this._log.error(err, "Unable to deleteMany", this._dbName, report);
-              });
             coll.insertOne(report, err => {
                 if (err)
                     this._log.error(err, "Unable to insert", this._dbName, report);
@@ -178,11 +173,6 @@ export class Persister<T extends Persistable> implements ILoadAll<T> {
     public persist = (report: T) => {
         this.collection.then(coll => {
             this._saver(report);
-            if (this._dbName != 'trades')
-              coll.deleteMany({ time: { $exists:true } }, err => {
-                  if (err)
-                      this._log.error(err, "Unable to deleteMany", this._dbName, report);
-              });
             coll.insertOne(report, err => {
                 if (err)
                     this._log.error(err, "Unable to insert", this._dbName, report);
