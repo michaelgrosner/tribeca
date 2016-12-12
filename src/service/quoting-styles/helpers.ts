564731ff6e759bea0c6c4032a88d14656077259a
Carles Tubio <ctubio@users.noreply.github.com>
2016-12-12 15:20:24 +0000

    Updated ping-pong styles out of top-join scope.

--- src/service/quoting-styles/helpers.ts
+++ src/service/quoting-styles/helpers.ts
@@ -10,3 +10,15 @@ export interface QuoteStyle {
     Mode : Models.QuotingMode;
     GenerateQuote(market: Models.Market, fv: Models.FairValue, params: Models.QuotingParameters) : GeneratedQuote;
 }
+
+export function getQuoteAtTopOfMarket(filteredMkt: Models.Market, params: Models.QuotingParameters): GeneratedQuote {
+    var topBid = (filteredMkt.bids[0].size > params.stepOverSize ? filteredMkt.bids[0] : filteredMkt.bids[1]);
+    if (typeof topBid === "undefined") topBid = filteredMkt.bids[0]; // only guaranteed top level exists
+    var bidPx = topBid.price;
+
+    var topAsk = (filteredMkt.asks[0].size > params.stepOverSize ? filteredMkt.asks[0] : filteredMkt.asks[1]);
+    if (typeof topAsk === "undefined") topAsk = filteredMkt.asks[0];
+    var askPx = topAsk.price;
+
+    return new GeneratedQuote(bidPx, topBid.size, askPx, topAsk.size);
+}
