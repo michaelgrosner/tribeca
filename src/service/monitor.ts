aabfe87fc8587cabad39e1b9e423db479806986d
Carles Tubio <ctubio@users.noreply.github.com>
2016-12-25 21:37:40 +0000

    Added auto save on change to notepad.

--- src/service/monitor.ts
+++ src/service/monitor.ts
@@ -16,6 +16,8 @@ export class ApplicationState {
 
     private _app_state : Models.ApplicationState = null;
 
+    private _notepad : Models.Notepad = null;
+
     private onTick = () => {
       this._app_state = new Models.ApplicationState(
         process.memoryUsage().rss,
@@ -25,10 +27,21 @@ export class ApplicationState {
     };
 
     constructor(private _timeProvider: Utils.ITimeProvider,
-                private _appStatePublisher : Messaging.IPublish<Models.ApplicationState>) {
+                private _appStatePublisher : Messaging.IPublish<Models.ApplicationState>,
+                private _notepadPublisher : Messaging.IPublish<Models.Notepad>,
+                private _changeNotepadReciever : Messaging.IReceive<Models.Notepad>) {
         _timeProvider.setInterval(this.onTick, moment.duration(1, "minutes"));
         this.onTick();
 
         _appStatePublisher.registerSnapshot(() => [this._app_state]);
+        _notepadPublisher.registerSnapshot(() => [this._notepad]);
+
+        _changeNotepadReciever.registerReceiver((notepad : Models.Notepad) => {
+            // this._log.info("handling change notepad request");
+            this._notepad = notepad;
+                // .then(x => this._log.info("changed notepad", x),
+                      // e => this._log.error(e, "error when changing notepad!"))
+                      ;
+        });
     }
 }
