8a59d8efaa1215fe4a31e936ce156f0136c58280
Carles Tubio <ctubio@users.noreply.github.com>
2017-01-02 03:21:29 +0000

    Updated npm modules.

--- src/service/main.ts
+++ src/service/main.ts
@@ -30,7 +30,7 @@ import request = require('request');
 import http = require("http");
 import https = require('https');
 import socket_io = require('socket.io');
-var marked = require('marked');
+import marked = require('marked');
 // var heapdump = require('heapdump'); // kill -USR2
 
 import HitBtc = require("./gateways/hitbtc");
