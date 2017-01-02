84c8f8e35aad3f61f0029d9b760316f987197111
Carles Tubio <ctubio@users.noreply.github.com>
2017-01-02 13:09:05 +0000

    Added dummy chart component.

---
+++ src/admin/fv-chart.ts
@@ -0,0 +1,64 @@
+/// <reference path="../common/models.ts" />
+/// <reference path="../common/messaging.ts" />
+/// <reference path="shared_directives.ts"/>
+
+import angular = require("angular");
+import Models = require("../common/models");
+import io = require("socket.io-client");
+import moment = require("moment");
+import Messaging = require("../common/messaging");
+import Shared = require("./shared_directives");
+//import Highcharts = require('highcharts-ng');
+
+interface fvChartScope extends ng.IScope {
+    fairValueChart: Models.FairValue[];
+}
+
+var fvChartController = ($scope: fvChartScope,
+                           $log: ng.ILogService,
+                           subscriberFactory: Shared.SubscriberFactory) => {
+    $scope.fairValueChart = [];
+
+    var clear = () => {
+        $scope.fairValueChart.length = 0;
+    };
+
+    var addFairValue = (fv: Models.FairValue) => {
+        if (fv == null) {
+            clear();
+            return;
+        }
+
+        //$scope.fairValueChart.push(fv);
+    };
+
+    var sub = subscriberFactory.getSubscriber($scope, Messaging.Topics.FairValue)
+        .registerConnectHandler(clear)
+        .registerDisconnectedHandler(clear)
+        .registerSubscriber(addFairValue, fv => fv.forEach(addFairValue));
+
+    $scope.$on('$destroy', () => {
+        sub.disconnect();
+        // $log.info("destroy order list");
+    });
+
+    // $log.info("started order list");
+};
+
+var fvChart = (): ng.IDirective => {
+    var template = '<div></div>';
+
+    return {
+        template: template,
+        restrict: "E",
+        replace: true,
+        transclude: false,
+        controller: fvChartController
+    }
+};
+
+export var fvChartDirective = "fvChartDirective";
+
+angular.module(fvChartDirective, ['sharedDirectives'])
+    .controller('fvChartController', fvChartController)
+    .directive("fvChart", fvChart);
