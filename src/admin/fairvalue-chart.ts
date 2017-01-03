a8c2e7dd43c099d6a0ba43f0799d3bcad4cf4bc4
Carles Tubio <ctubio@users.noreply.github.com>
2017-01-03 21:01:57 +0000

    Updated aligned directives without useless imports.

--- src/admin/fairvalue-chart.ts
+++ src/admin/fairvalue-chart.ts
@@ -4,8 +4,6 @@
 
 import angular = require('angular');
 import Models = require('../common/models');
-import io = require('socket.io-client');
-import moment = require('moment');
 import Messaging = require('../common/messaging');
 import Shared = require('./shared_directives');
 //import Highcharts = require('highcharts-ng');
