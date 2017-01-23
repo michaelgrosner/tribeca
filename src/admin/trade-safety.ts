c34ac7f711241b35a18a63647166a32187310e60
Carles Tubio <ctubio@users.noreply.github.com>
2017-01-23 13:26:12 +0000

    Added 3 decimal digits to TBP.

--- src/admin/trade-safety.ts
+++ src/admin/trade-safety.ts
@@ -13,7 +13,7 @@ import {SubscriberFactory} from './shared_directives';
   template: `<div div class="tradeSafety img-rounded">
       <div>BuyPing: <span class="{{ buySizeSafety ? \'text-danger\' : \'text-muted\' }}">{{ buySizeSafety | number:'1.2-2' }}</span>,
       SellPing: <span class="{{ sellSizeSafety ? \'text-danger\' : \'text-muted\' }}">{{ sellSizeSafety | number:'1.2-2' }}</span>,
-      Target Base Position: <span class="text-danger">{{ targetBasePosition | number:'1.2-2' }}</span>,
+      Target Base Position: <span class="text-danger">{{ targetBasePosition | number:'1.3-3' }}</span>,
       BuyTS: {{ buySafety | number:'1.2-2' }},
       SellTS: {{ sellSafety | number:'1.2-2' }},
       TotalTS: {{ tradeSafetyValue | number:'1.2-2' }}</div>
