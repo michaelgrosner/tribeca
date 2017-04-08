0e08b2378dc729bddbbbf983bdb0cc5bdd574f4c
Carles Tubio <ctubio@users.noreply.github.com>
2017-04-08 23:46:44 +0000

    Fixed new OkCoin API issues to resolve #26.
    To support commits at ctubio/tribeca,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/server/gateways/okcoin.ts
+++ src/server/gateways/okcoin.ts
@@ -156,14 +156,14 @@ class OkCoinMarketDataGateway implements Interfaces.IMarketDataGateway {
 
     MarketData = new Utils.Evt<Models.Market>();
 
-    private static GetLevel = (n: [number, number]) : Models.MarketSide =>
-        new Models.MarketSide(n[0], n[1]);
+    private static GetLevel = (n: [any, any]) : Models.MarketSide =>
+        new Models.MarketSide(parseFloat(n[0]), parseFloat(n[1]));
 
     private onDepth = (depth : Models.Timestamped<OkCoinDepthMessage>) => {
         var msg = depth.data;
 
         var bids = _(msg.bids).take(13).map(OkCoinMarketDataGateway.GetLevel).value();
-        var asks = _(msg.asks).reverse().take(13).map(OkCoinMarketDataGateway.GetLevel).value()
+        var asks = _(msg.asks).reverse().take(13).map(OkCoinMarketDataGateway.GetLevel).value();
         var mkt = new Models.Market(bids, asks, depth.time);
 
         this.MarketData.trigger(mkt);
