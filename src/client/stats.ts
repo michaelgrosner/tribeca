f53f5016ffb8c05ba53116d48c4d0f236ad4e0c8
Carles Tubio <ctubio@users.noreply.github.com>
2017-04-14 19:59:07 +0000

    Added charts to resolve #33.
    To support commits at ctubio/tribeca,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/client/stats.ts
+++ src/client/stats.ts
@@ -1,31 +1,183 @@
 import {NgZone, Component, Inject, OnInit} from '@angular/core';
+import moment = require('moment');
 
 import Models = require('../share/models');
 import {SubscriberFactory} from './shared_directives';
 
 @Component({
   selector: 'market-stats',
-  template: `<chart type="StockChart" [options]="chartOptions" (load)="saveInstance($event.context)"></chart>`
+  template: `<div class="col-md-6 col-xs-6">
+    <chart style="height:400px;width:700px;" type="StockChart" [options]="fvChartOptions" (load)="saveInstance($event.context, 'fv')"></chart>
+  </div>
+  <div class="col-md-6 col-xs-6">
+    <chart style="height:200px;width:700px;" [options]="baseChartOptions" (load)="saveInstance($event.context, 'base')"></chart>
+    <chart style="height:200px;width:700px;" [options]="quoteChartOptions" (load)="saveInstance($event.context, 'quote')"></chart>
+  </div>`
 })
 export class StatsComponent implements OnInit {
 
   public fairValue: number;
-  public chart: any;
-  private saveInstance = (chartInstance) => {
-    this.chart = chartInstance;
+  public fvChart: any;
+  public quoteChart: any;
+  public baseChart: any;
+  private saveInstance = (chartInstance, chartId) => {
+    this[chartId+'Chart'] = chartInstance;
   }
-  public chartOptions = {
-    title: { text: 'fair value and trades' },
+  public fvChartOptions = {
+    title: 'fair value',
+    chart: {
+        zoomType: 'x',
+        backgroundColor:'rgba(255, 255, 255, 0)'
+    },
+    xAxis: {
+      type: 'datetime'
+    },
+    yAxis: {
+      title: {
+        text: 'Fair Value and Trades'
+      }
+    },
+    tooltip: {
+        split: true
+    },
+    legend: {
+        enabled: false
+    },
     series: [{
       name: 'Fair Value',
+      type: 'spline',
+      colorIndex: 2,
       data: [],
       id: 'fvseries'
     },{
-      type: 'flags',
+      name: 'Sell',
+      type: 'scatter',
+      colorIndex: 5,
       data: [],
-      onSeries: 'fvseries',
+      id: 'sellseries'
+    },{
+      type: 'flags',
+      colorIndex: 5,
+      data: [],
+      onSeries: 'sellseries',
       shape: 'circlepin',
       width: 16
+    },{
+      name: 'Buy',
+      type: 'scatter',
+      colorIndex: 0,
+      data: [],
+      id: 'buyseries'
+    },{
+      type: 'flags',
+      colorIndex: 0,
+      data: [],
+      onSeries: 'buyseries',
+      shape: 'circlepin',
+      width: 16
+    }]
+  };
+  public quoteChartOptions = {
+    title: 'quote wallet',
+    chart: {
+        zoomType: 'x',
+        backgroundColor:'rgba(255, 255, 255, 0)'
+    },
+    tooltip: {
+        split: true,
+        pointFormat:'<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:.2f} €</b><br/>'
+    },
+    plotOptions: {
+        series: {
+            stacking: 'normal'
+        }
+    },
+    xAxis: {
+      type: 'datetime'
+    },
+    yAxis: [{
+      title: {
+        text: 'Total Position'
+      },
+      opposite: true
+    },{
+      title: {
+        text: 'Available and Held'
+      },
+    }],
+    legend: {
+        enabled: false
+    },
+    series: [{
+      name: 'Available',
+      type: 'column',
+      colorIndex:1,
+      yAxis: 1,
+      data: []
+    },{
+      name: 'Held',
+      type: 'column',
+      colorIndex:0,
+      yAxis: 1,
+      data: []
+    },{
+      name: 'Total Position',
+      type: 'spline',
+      colorIndex:2,
+      yAxis: 0,
+      data: []
+    }]
+  };
+  public baseChartOptions = {
+    title: 'base wallet',
+    type: 'column',
+    chart: {
+        zoomType: 'x',
+        backgroundColor:'rgba(255, 255, 255, 0)'
+    },
+    tooltip: {
+        split: true,
+        pointFormat:'<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:.8f} ฿</b><br/>'
+    },
+    plotOptions: {
+        series: {
+            stacking: 'normal'
+        }
+    },
+    xAxis: {
+      type: 'datetime'
+    },
+    yAxis: [{
+      title: {
+        text: 'Total Position'
+      },
+      opposite: true
+    },{
+      title: {
+        text: 'Available and Held'
+      }
+    }],
+    legend: {
+        enabled: false
+    },
+    series: [{
+      name: 'Available',
+      type: 'column',
+      yAxis: 1,
+      colorIndex:1,
+      data: []
+    },{
+      name: 'Held',
+      type: 'column',
+      yAxis: 1,
+      colorIndex:5,
+      data: []
+    },{
+      name: 'Total Position',
+      type: 'spline',
+      yAxis: 0,
+      colorIndex:2,
+      data: []
     }]
   };
 
@@ -40,23 +192,47 @@ export class StatsComponent implements OnInit {
       .registerSubscriber(this.updateFairValue);
 
     this.subscriberFactory
-      .getSubscriber(this.zone, Models.Topics.OrderStatusReports)
-      .registerSubscriber(this.addRowData);
+      .getSubscriber(this.zone, Models.Topics.TradesChart)
+      .registerSubscriber(this.addTradesChartData);
+
+    this.subscriberFactory
+      .getSubscriber(this.zone, Models.Topics.WalletChart)
+      .registerSubscriber(this.addWalletChartData);
   }
 
   private updateFairValue = (fv: Models.FairValue) => {
     if (fv == null) return;
-
-    this.chart.series[0].addPoint([Date.now(), fv.price]);
+    this.fvChart.series[0].addPoint([
+      new Date().getTime(),
+      ((fv.price * 100) / 100)
+    ]);
   }
 
-  private addRowData = (o: Models.Timestamped<any[]>) => {
-    if (o.data[1] == Models.OrderStatus.Complete)
-      this.chart.series[1].addPoint({
-          x: Date.now(),
-          title: Models.Side[o.data[2]] == 'Bid' ? 'buy' : 'sell',
-          useHTML:true,
-          text: 'Price: ' + o.data[3] + '<br/>' + 'Qty: ' + ((o.data[4] * 100) / 100)
-      });
+  private addTradesChartData = (t: Models.TradeChart) => {
+    let time = new Date().getTime();
+    this.fvChart.series[Models.Side[t.side] == 'Bid' ? 3 : 1].addPoint([
+      time,
+      ((t.price * 100) / 100)
+    ]);
+    this.fvChart.series[Models.Side[t.side] == 'Bid' ? 4 : 2].addPoint({
+      x: time,
+      title: Models.Side[t.side] == 'Bid' ? 'B' : 'S',
+      lineColor: Models.Side[t.side] == 'Bid' ? '#FF0000' : '#0000FF',
+      useHTML:true,
+      text: 'Price: ' + ((t.price * 100) / 100)
+        + '<br/>' + 'Qty: ' + t.quantity
+        + '<br/>' + 'Value: ' + ((t.value * 100) / 100)
+    });
+  }
+
+  private addWalletChartData = (w: Models.WalletChart) => {
+    let time = new Date().getTime();
+    this.fvChart.series[0].addPoint([time, w.fairValue]);
+    this.quoteChart.series[0].addPoint([time, w.availQuote]);
+    this.quoteChart.series[1].addPoint([time, w.heldQuote]);
+    this.quoteChart.series[2].addPoint([time, w.totalQuote]);
+    this.baseChart.series[0].addPoint([time, w.availBase]);
+    this.baseChart.series[1].addPoint([time, w.heldBase]);
+    this.baseChart.series[2].addPoint([time, w.totalBase]);
   }
 }
