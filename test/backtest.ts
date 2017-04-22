c94447cdca391d9e0e9184854e6891dbe294255c
Carles Tubio <ctubio@users.noreply.github.com>
2017-04-22 23:51:11 +0000

    Updated normalized import of moment.
    To support commits at ctubio/tribeca,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- test/backtest.ts
+++ test/backtest.ts
@@ -4,87 +4,87 @@ import Interfaces = require("../src/server/interfaces");
 import Utils = require("../src/server/utils");
 import Persister = require("../src/server/persister");
 import Models = require("../src/share/models");
-import Moment = require("moment");
+import moment = require("moment");
 
 describe("BacktestTests", () => {
     var timeProvider : Backtest.BacktestTimeProvider;
 
     beforeEach(() => {
-        var t0 = Moment.unix(1);
-        var t1 = Moment.unix(10);
+        var t0 = moment.unix(1);
+        var t1 = moment.unix(10);
         timeProvider = new Backtest.BacktestTimeProvider(t0, t1);
     });
 
     it("Should increment time", () => {
-        timeProvider.scrollTimeTo(Moment.unix(2));
-        assert.equal(timeProvider.utcNow().diff(Moment.unix(2)), 0);
+        timeProvider.scrollTimeTo(moment.unix(2));
+        assert.equal(timeProvider.utcNow().diff(moment.unix(2)), 0);
     });
 
     it("Should not allow rewinding time", () => {
-        timeProvider.scrollTimeTo(Moment.unix(6));
-        assert.throws(() => timeProvider.scrollTimeTo(Moment.unix(2)));
+        timeProvider.scrollTimeTo(moment.unix(6));
+        assert.throws(() => timeProvider.scrollTimeTo(moment.unix(2)));
     });
 
     it("Should handle timeouts", () => {
         var triggered = false;
-        timeProvider.setTimeout(() => triggered = true, Moment.duration(4, "seconds"));
+        timeProvider.setTimeout(() => triggered = true, moment.duration(4, "seconds"));
 
-        timeProvider.scrollTimeTo(Moment.unix(2));
+        timeProvider.scrollTimeTo(moment.unix(2));
         assert.equal(triggered, false, "should not yet be triggered");
 
-        timeProvider.scrollTimeTo(Moment.unix(7));
+        timeProvider.scrollTimeTo(moment.unix(7));
         assert.equal(triggered, true, "should be triggered");
     });
 
     it("Should handle timeouts in order", () => {
         var triggeredFirst = false;
-        timeProvider.setTimeout(() => triggeredFirst = true, Moment.duration(4, "seconds"));
+        timeProvider.setTimeout(() => triggeredFirst = true, moment.duration(4, "seconds"));
 
         var triggeredSecond = false;
-        timeProvider.setTimeout(() => triggeredSecond = true, Moment.duration(7, "seconds"));
+        timeProvider.setTimeout(() => triggeredSecond = true, moment.duration(7, "seconds"));
 
-        timeProvider.scrollTimeTo(Moment.unix(2));
+        timeProvider.scrollTimeTo(moment.unix(2));
         assert.equal(triggeredFirst, false, "1 should not yet be triggered");
         assert.equal(triggeredSecond, false, "2 should not yet be triggered");
 
-        timeProvider.scrollTimeTo(Moment.unix(7));
+        timeProvider.scrollTimeTo(moment.unix(7));
         assert.equal(triggeredFirst, true, "1 should be triggered");
         assert.equal(triggeredSecond, false, "2 should not yet be triggered");
 
-        timeProvider.scrollTimeTo(Moment.unix(9));
+        timeProvider.scrollTimeTo(moment.unix(9));
         assert.equal(triggeredFirst, true, "1 should be triggered");
         assert.equal(triggeredSecond, true, "2 should be triggered");
     });
 
     it("Should handle intervals", () => {
         var nTimes = 0;
-        timeProvider.setInterval(() => nTimes += 1, Moment.duration(2, "seconds"));
+        timeProvider.setInterval(() => nTimes += 1, moment.duration(2, "seconds"));
 
-        timeProvider.scrollTimeTo(Moment.unix(9));
+        timeProvider.scrollTimeTo(moment.unix(9));
         assert.equal(nTimes, 3);
     });
 
     it("Should handle both intervals and timeouts", () => {
         var nTimes = 0;
-        timeProvider.setInterval(() => nTimes += 1, Moment.duration(2, "seconds"));
+        timeProvider.setInterval(() => nTimes += 1, moment.duration(2, "seconds"));
 
         var triggeredFirst = false;
-        timeProvider.setTimeout(() => triggeredFirst = true, Moment.duration(4, "seconds"));
+        timeProvider.setTimeout(() => triggeredFirst = true, moment.duration(4, "seconds"));
 
         var triggeredSecond = false;
-        timeProvider.setTimeout(() => triggeredSecond = true, Moment.duration(7, "seconds"));
+        timeProvider.setTimeout(() => triggeredSecond = true, moment.duration(7, "seconds"));
 
-        timeProvider.scrollTimeTo(Moment.unix(2));
+        timeProvider.scrollTimeTo(moment.unix(2));
         assert.equal(nTimes, 0);
         assert.equal(triggeredFirst, false, "1 should not yet be triggered");
         assert.equal(triggeredSecond, false, "2 should not yet be triggered");
 
-        timeProvider.scrollTimeTo(Moment.unix(7));
+        timeProvider.scrollTimeTo(moment.unix(7));
         assert.equal(nTimes, 2);
         assert.equal(triggeredFirst, true, "1 should be triggered");
         assert.equal(triggeredSecond, false, "2 should not yet be triggered");
 
-        timeProvider.scrollTimeTo(Moment.unix(9));
+        timeProvider.scrollTimeTo(moment.unix(9));
         assert.equal(nTimes, 3);
         assert.equal(triggeredFirst, true, "1 should be triggered");
         assert.equal(triggeredSecond, true, "2 should be triggered");
@@ -94,14 +94,14 @@ describe("BacktestTests", () => {
 describe("BacktestGatewayTests", () => {
     it("should read market data", () => {
         assert.equal(new Models.MarketSide(1, 2).toString(), 'px=1;size=2');
-        assert.equal(true, new Models.OrderStatusReportImpl(new Models.CurrencyPair(Models.Currency.EUR,Models.Currency.BTC),null,null,null,null,null,'1',null,null,null,Moment.unix(1),null,2,null,null,null,null,null,null,null,null,null,null,null,null,null).toString().indexOf('lastPrice=2')>-1);
+        assert.equal(true, new Models.OrderStatusReportImpl(new Models.CurrencyPair(Models.Currency.EUR,Models.Currency.BTC),null,null,null,null,null,'1',null,null,null,moment.unix(1),null,2,null,null,null,null,null,null,null,null,null,null,null,null,null).toString().indexOf('lastPrice=2')>-1);
 
         var inputData : Array<Models.Market | Models.MarketTrade> = [
-            new Models.Market([new Models.MarketSide(10, 5)], [new Models.MarketSide(20, 5)], Moment.unix(1)),
-            new Models.Market([new Models.MarketSide(15, 5)], [new Models.MarketSide(20, 5)], Moment.unix(10)),
+            new Models.Market([new Models.MarketSide(10, 5)], [new Models.MarketSide(20, 5)], moment.unix(1)),
+            new Models.Market([new Models.MarketSide(15, 5)], [new Models.MarketSide(20, 5)], moment.unix(10)),
         ];
 
-        var timeProvider = new Backtest.BacktestTimeProvider(Moment.unix(1), Moment.unix(40));
+        var timeProvider = new Backtest.BacktestTimeProvider(moment.unix(1), moment.unix(40));
         var gateway = new Backtest.BacktestGateway(inputData, 10, 5000, timeProvider);
 
         gateway.MarketData.once(m => {
