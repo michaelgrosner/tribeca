8781a7517752b4fdec8fc7e350bc31952bce9050
Carles Tubio <ctubio@users.noreply.github.com>
2017-04-22 23:51:31 +0000

    Updated source without useless vars.
    To support commits at ctubio/tribeca,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/server/quoting-styles/helpers.ts
+++ src/server/quoting-styles/helpers.ts
@@ -2,22 +2,18 @@ import Interfaces = require("../interfaces");
 import Models = require("../../share/models");
 
 export class GeneratedQuote {
-    constructor(public bidPx: number, public bidSz: number, public askPx: number, public askSz: number) { }
+  constructor(public bidPx: number, public bidSz: number, public askPx: number, public askSz: number) { }
 }
 
 export interface QuoteStyle {
-    Mode : Models.QuotingMode;
-    GenerateQuote(market: Models.Market, fv: Models.FairValue, params: Models.QuotingParameters, position:Interfaces.IPositionBroker) : GeneratedQuote;
+  Mode : Models.QuotingMode;
+  GenerateQuote(market: Models.Market, fv: Models.FairValue, params: Models.QuotingParameters, position:Interfaces.IPositionBroker) : GeneratedQuote;
 }
 
 export function getQuoteAtTopOfMarket(filteredMkt: Models.Market, params: Models.QuotingParameters): GeneratedQuote {
-    var topBid = (filteredMkt.bids[0].size > params.stepOverSize ? filteredMkt.bids[0] : filteredMkt.bids[1]);
-    if (typeof topBid === "undefined") topBid = filteredMkt.bids[0]; // only guaranteed top level exists
-    var bidPx = topBid.price;
-
-    var topAsk = (filteredMkt.asks[0].size > params.stepOverSize ? filteredMkt.asks[0] : filteredMkt.asks[1]);
-    if (typeof topAsk === "undefined") topAsk = filteredMkt.asks[0];
-    var askPx = topAsk.price;
-
-    return new GeneratedQuote(bidPx, topBid.size, askPx, topAsk.size);
+  let topBid = (filteredMkt.bids[0].size > params.stepOverSize ? filteredMkt.bids[0] : filteredMkt.bids[1]);
+  if (typeof topBid === "undefined") topBid = filteredMkt.bids[0];
+  let topAsk = (filteredMkt.asks[0].size > params.stepOverSize ? filteredMkt.asks[0] : filteredMkt.asks[1]);
+  if (typeof topAsk === "undefined") topAsk = filteredMkt.asks[0];
+  return new GeneratedQuote(topBid.price, topBid.size, topAsk.price, topAsk.size);
 }
