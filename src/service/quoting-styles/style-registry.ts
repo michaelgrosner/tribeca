5faeab842d774f49c1bc3121bb463632196c430f
Michael Grosner <michaelgrosner@gmail.com>
2017-04-22 15:29:48 -0400

    Round prices in the quote generators

--- src/service/quoting-styles/style-registry.ts
+++ src/service/quoting-styles/style-registry.ts
@@ -7,7 +7,7 @@ import _ = require("lodash");
 class NullQuoteGenerator implements StyleHelpers.QuoteStyle {
 	Mode = null;
     
-    GenerateQuote = (market: Models.Market, fv: Models.FairValue, params: Models.QuotingParameters) : StyleHelpers.GeneratedQuote => {
+    GenerateQuote = (input: StyleHelpers.QuoteInput) : StyleHelpers.GeneratedQuote|null => {
 		return null;
 	};
 }
