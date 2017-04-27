125ecbaeae95157348a5dbe09d75ac7c6aea5a66
Carles Tubio <ctubio@users.noreply.github.com>
2017-04-27 23:15:20 +0000

    Fixed size too precise error in Coinbase to resolve #47.
    To support commits at ctubio/tribeca,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/server/gateways/coinbase.ts
+++ src/server/gateways/coinbase.ts
@@ -572,7 +572,7 @@ class CoinbaseOrderEntryGateway implements Interfaces.IOrderEntryGateway {
 
         var o: CoinbaseOrder = {
             client_oid: order.orderId,
-            size: order.quantity.toString(),
+            size: order.quantity.toFixed(8).toString(),
             product_id: this._symbolProvider.symbol
         };
 
