40e364f4a10f6657e0945a18cbcebded01764ac6
Carles Tubio <ctubio@users.noreply.github.com>
2017-05-02 17:28:25 +0000

    Updated source with log file again.
    To support commits at ctubio/tribeca,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/server/logging.ts
+++ src/server/logging.ts
@@ -1,20 +1,23 @@
 import bunyan = require("bunyan");
 import * as _ from "lodash";
+import fs = require("fs");
 
 export default function log(name: string) : bunyan {
-    // don't log while testing
-    const isRunFromMocha = process.argv.length >= 2 && _.includes(process.argv[1], "mocha");
-    if (isRunFromMocha) {
+    if (process.argv.length >= 2 && _.includes(process.argv[1], "mocha"))
         return bunyan.createLogger({name: name, stream: process.stdout, level: bunyan.FATAL});
-    }
 
-    let level = "info";
-    if (_.includes(process.argv, "debug")) {
-        level = "debug";
-    }
+    if (!fs.existsSync('./log')) fs.mkdirSync('./log');
+
+    const level = _.includes(process.argv, 'debug') ? 'debug' : 'info';
 
     return bunyan.createLogger({
         name: name,
-        streams: [{ level: level, stream: process.stdout }]
+        streams: [{
+            level: level,
+            stream: process.stdout
+        }, {
+            level: level,
+            path: './log/tribeca.log'
+        }]
     });
 }
