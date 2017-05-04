a14413dd5ed233528c4ac0c25f879e29715b8b75
Carles Tubio <ctubio@users.noreply.github.com>
2017-05-04 22:03:21 +0000

    Updated save of parameters.
    To support commits at ctubio/tribeca,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/server/quoting-parameters.ts
+++ src/server/quoting-parameters.ts
@@ -13,6 +13,8 @@ class Repository<T> implements Interfaces.IRepository<T> {
     private _name: string,
     private _validator: (a: T) => boolean,
     private _paramsEqual: (a: T, b: T) => boolean,
+    private _paramsEqualUI: (a: T, b: T) => T,
+    private _paramsPersist: () => void,
     defaultParameter: T,
     private _rec: Publish.IReceive<T>,
     private _pub: Publish.IPublish<T>
@@ -28,9 +30,13 @@ class Repository<T> implements Interfaces.IRepository<T> {
   }
 
   public updateParameters = (newParams: T) => {
-    if (this._validator(newParams) && this._paramsEqual(newParams, this._latest)) {
-      this._latest = newParams;
-      this.NewParameters.trigger();
+    if (this._validator(newParams)) {
+      this._latest = this._paramsEqualUI(newParams, this._latest);
+      if (this._paramsEqual(newParams, this._latest)) {
+        this._latest = newParams;
+        this.NewParameters.trigger();
+      }
+      this._paramsPersist();
     }
 
     this._pub.publish(this.latest);
@@ -46,8 +52,9 @@ export class QuotingParametersRepository extends Repository<Models.QuotingParame
   ) {
     super("qpr",
       (p: Models.QuotingParameters) => p.buySize > 0 || p.sellSize > 0 || p.buySizePercentage > 0 || p.sellSizePercentage > 0 || p.widthPing > 0 || p.widthPong > 0,
-      (a: Models.QuotingParameters, b: Models.QuotingParameters) => !_.isEqual(a, Object.assign(b, {audio:a.audio, delayUI:a.delayUI})),
+      (a: Models.QuotingParameters, b: Models.QuotingParameters) => !_.isEqual(a, b),
+      (a: Models.QuotingParameters, b: Models.QuotingParameters) => Object.assign(b, {audio:a.audio, delayUI:a.delayUI}),
+      () => paramsPersister.persist(this.latest),
       initParam, rec, pub);
-    this.NewParameters.on(() => paramsPersister.persist(this.latest));
   }
 }
