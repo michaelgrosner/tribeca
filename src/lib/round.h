2f8c29c83a75a23d999c49f341ee32eeac286909
Carles Tubio <ctubio@users.noreply.github.com>
2017-05-21 19:39:01 +0000

    Added namespace to C++ shared object.
    To support commits at ctubio/tribeca,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/lib/round.h
+++ src/lib/round.h
@@ -1,22 +1,25 @@
-#ifndef ROUND_H
-#define ROUND_H
-
-#include <math.h>
+#ifndef TRIBECA_ROUND_
+#define TRIBECA_ROUND_
 
 #include <nan.h>
-using v8::Local;
-using v8::Object;
+#include <math.h>
 
+namespace tribeca {
+  using v8::Local;
+  using v8::Value;
+  using v8::Number;
+  using v8::Object;
 
-class Round {
-  public:
-    static void Init(v8::Local<v8::Object> target);
-    static NAN_METHOD(RoundUp);
-    static NAN_METHOD(RoundDown);
-    static NAN_METHOD(RoundNearest);
-    static NAN_METHOD(RoundSide);
-  private:
-    static bool Valid(const Nan::FunctionCallbackInfo<v8::Value>& info);
-};
+  class Round {
+    public:
+      static void Init(v8::Local<v8::Object> target);
+      static NAN_METHOD(RoundUp);
+      static NAN_METHOD(RoundDown);
+      static NAN_METHOD(RoundNearest);
+      static NAN_METHOD(RoundSide);
+    private:
+      static bool Valid(const Nan::FunctionCallbackInfo<v8::Value>& info);
+  };
+}
 
 #endif
