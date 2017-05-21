03e11901f93257523972fe207309e4c4c8729b23
Carles Tubio <ctubio@users.noreply.github.com>
2017-05-21 00:36:19 +0000

    Fixed price precision.
    To support commits at ctubio/tribeca,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/server/quoting-engine.ts
+++ src/server/quoting-engine.ts
@@ -254,10 +254,10 @@ export class QuotingEngine {
         }
 
         if (unrounded.askSz !== null)
-            unrounded.askSz = Math.max(minSize, Utils.roundDown(unrounded.askSz, 1e-8));
+            unrounded.askSz = Utils.roundDown(Math.max(minSize, unrounded.askSz), 1e-8);
 
           if (unrounded.bidSz !== null)
-            unrounded.bidSz = Math.max(minSize, Utils.roundDown(unrounded.bidSz, 1e-8));
+            unrounded.bidSz = Utils.roundDown(Math.max(minSize, unrounded.bidSz), 1e-8);
 
         return unrounded;
     }
