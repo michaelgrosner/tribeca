e67528cd3001ff08a988af33e61ec5712e59d74b
Carles Tubio <ctubio@users.noreply.github.com>
2017-05-22 23:30:48 +0000

    Updated source without useless conditions.
    To support commits at ctubio/tribeca,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/lib/round.cc
+++ src/lib/round.cc
@@ -9,10 +9,8 @@ namespace tribeca {
   }
 
   NAN_METHOD(Round::RoundUp) {
-    if (!Round::Valid(info)) return;
-
-    float value = info[0]->NumberValue();
-    float minTick = info[1]->NumberValue();
+    double value = info[0]->NumberValue();
+    double minTick = info[1]->NumberValue();
 
     v8::Local<v8::Number> num = Nan::New(ceil(value / minTick) * minTick);
 
@@ -20,10 +18,8 @@ namespace tribeca {
   }
 
   NAN_METHOD(Round::RoundDown) {
-    if (!Round::Valid(info)) return;
-
-    float value = info[0]->NumberValue();
-    float minTick = info[1]->NumberValue();
+    double value = info[0]->NumberValue();
+    double minTick = info[1]->NumberValue();
 
     v8::Local<v8::Number> num = Nan::New(floor(value / minTick) * minTick);
 
@@ -31,10 +27,8 @@ namespace tribeca {
   }
 
   NAN_METHOD(Round::RoundNearest) {
-    if (!Round::Valid(info)) return;
-
-    float value = info[0]->NumberValue();
-    float minTick = info[1]->NumberValue();
+    double value = info[0]->NumberValue();
+    double minTick = info[1]->NumberValue();
 
     v8::Local<v8::Number> num = Nan::New(round(value / minTick) * minTick);
 
@@ -42,33 +36,11 @@ namespace tribeca {
   }
 
   NAN_METHOD(Round::RoundSide) {
-    if (!Round::Valid(info)) return;
-
-    if (!info[2]->IsNumber()) {
-      Nan::ThrowTypeError("Wrong side value");
-      return;
-    }
-
     int side = info[2]->NumberValue();
-
     switch (side) {
       case 0: Round::RoundDown(info); break;
       case 1: Round::RoundUp(info); break;
       default: Round::RoundNearest(info); break;
     }
-  }
-
-  bool Round::Valid(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-    if (info.Length() < 2) {
-      Nan::ThrowTypeError("Wrong number of arguments");
-      return false;
-    }
-
-    if (!info[0]->IsNumber() || !info[1]->IsNumber()) {
-      Nan::ThrowTypeError("Wrong arguments");
-      return false;
-    }
-
-    return true;
   }
 }
