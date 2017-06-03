49149ace6fb372da70d7ba5b83ff7cbdb7cefed8
Carles Tubio <ctubio@users.noreply.github.com>
2017-06-03 17:28:32 +0000

    Fixed dummy test for ewma.
    To support commits at ctubio/tribeca,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- test/statistics_tests.ts
+++ test/statistics_tests.ts
@@ -7,8 +7,7 @@ describe("EwmaStatisticCalculator", () => {
         var input = [268.05, 258.73, 239.82, 250.21, 224.49, 242.53, 248.25, 270.58, 252.77, 273.55,
                      255.9, 226.1, 225, 263.12, 218.36, 254.73, 218.65, 252.4, 296.1, 222.2];
 
-        var calc = new Stats.EwmaStatisticCalculator(.095, <Models.RegularFairValue[]>input.map(x => new Models.RegularFairValue(new Date(), x)));
-
+        var calc = new Stats.EwmaStatisticCalculator(20.0526, <Models.RegularFairValue[]>input.map(x => new Models.RegularFairValue(new Date(), x)));
         assert(Math.abs(calc.latest - 249.901486) < 1e-5);
     });
 });
