bfc61d188752bdd6547a5ec40a2dc9eb19e5eb7c
Carles Tubio <ctubio@users.noreply.github.com>
2017-06-06 01:00:16 +0000

    Updaped source without useless class.
    To support commits at ctubio/tribeca,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/server/quoting-parameters.ts
+++ src/server/quoting-parameters.ts
@@ -5,50 +5,40 @@ import Publish = require("./publish");
 import _ = require("lodash");
 import Utils = require("./utils");
 
-class Repository<T> implements Interfaces.IRepository<T> {
-
+export class QuotingParametersRepository implements Interfaces.IRepository<Models.QuotingParameters> {
   NewParameters = new Utils.Evt();
 
   constructor(
-    private _name: string,
-    private _validator: (a: T) => boolean,
-    private _paramsEqual: (a: T, b: T) => boolean,
-    defaultParameter: T,
-    private _rec: Publish.IReceive<T>,
-    private _pub: Publish.IPublish<T>
+    private _paramsPersister: Persister.IPersist<Models.QuotingParameters>,
+    private _pub: Publish.IPublish<Models.QuotingParameters>,
+    rec: Publish.IReceive<Models.QuotingParameters>,
+    initParams: Models.QuotingParameters,
   ) {
     if (_pub) _pub.registerSnapshot(() => [this.latest]);
-    if (_rec) _rec.registerReceiver(this.updateParameters);
-    this._latest = defaultParameter;
+    if (rec) rec.registerReceiver(this.updateParameters);
+    this._latest = initParams;
   }
 
-  private _latest: T;
-  public get latest(): T {
+  private _validator: (a: Models.QuotingParameters) => boolean = (p: Models.QuotingParameters) => p.buySize > 0 || p.sellSize > 0 || p.buySizePercentage > 0 || p.sellSizePercentage > 0 || p.widthPing > 0 || p.widthPong > 0 || p.widthPingPercentage > 0 || p.widthPongPercentage > 0;
+
+  private _paramsEqual: (a: Models.QuotingParameters, b: Models.QuotingParameters) => boolean = (a: Models.QuotingParameters, b: Models.QuotingParameters) => !_.isEqual(a, b);
+
+  private _latest: Models.QuotingParameters;
+  public get latest(): Models.QuotingParameters {
     return this._latest;
   }
 
-  public updateParameters = (newParams: T) => {
+  public updateParameters = (newParams: Models.QuotingParameters) => {
     if (this._validator(newParams) && this._paramsEqual(newParams, this._latest)) {
-      if ((<any>newParams).mode===Models.QuotingMode.Depth) (<any>newParams).widthPercentage = false;
+
+      if (newParams.mode===Models.QuotingMode.Depth)
+        newParams.widthPercentage = false;
+
       this._latest = newParams;
+      this._paramsPersister.persist(newParams)
       this.NewParameters.trigger();
     }
 
     this._pub.publish(this.latest);
   };
-}
-
-export class QuotingParametersRepository extends Repository<Models.QuotingParameters> {
-  constructor(
-    pub: Publish.IPublish<Models.QuotingParameters>,
-    rec: Publish.IReceive<Models.QuotingParameters>,
-    initParam: Models.QuotingParameters,
-    paramsPersister: Persister.IPersist<Models.QuotingParameters>
-  ) {
-    super("qpr",
-      (p: Models.QuotingParameters) => p.buySize > 0 || p.sellSize > 0 || p.buySizePercentage > 0 || p.sellSizePercentage > 0 || p.widthPing > 0 || p.widthPong > 0 || p.widthPingPercentage > 0 || p.widthPongPercentage > 0,
-      (a: Models.QuotingParameters, b: Models.QuotingParameters) => !_.isEqual(a, b),
-      initParam, rec, pub);
-    this.NewParameters.on(() => paramsPersister.persist(this.latest));
-  }
 }
