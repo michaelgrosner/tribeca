df8765ee069316f86fbe5b2842aafe874e590c25
Carles Tubio <ctubio@users.noreply.github.com>
2017-06-22 20:11:25 +0000

    Fixed quoter switch side cos compilation mangle bug.
    Free Software Free Society
    
    To support commits by ctubio,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/server/quote-sender.ts
+++ src/server/quote-sender.ts
@@ -7,68 +7,69 @@ import Active = require("./active-state");
 import QuotingEngine = require("./quoting-engine");
 
 export class QuoteSender {
-    private _latest = new Models.TwoSidedQuoteStatus(Models.QuoteStatus.Held, Models.QuoteStatus.Held);
-    public get latestStatus() { return this._latest; }
-    public set latestStatus(val: Models.TwoSidedQuoteStatus) {
-        if (val.bidStatus === this._latest.bidStatus && val.askStatus === this._latest.askStatus) return;
+  private _latest = new Models.TwoSidedQuoteStatus(Models.QuoteStatus.Held, Models.QuoteStatus.Held);
+  public get latestStatus() { return this._latest; }
+  public set latestStatus(val: Models.TwoSidedQuoteStatus) {
+    if (val.bidStatus === this._latest.bidStatus && val.askStatus === this._latest.askStatus) return;
 
-        this._latest = val;
-        this._publisher.publish(Models.Topics.QuoteStatus, this._latest, true);
+    this._latest = val;
+    this._publisher.publish(Models.Topics.QuoteStatus, this._latest, true);
+  }
+
+  constructor(
+    private _timeProvider: Utils.ITimeProvider,
+    private _quotingEngine: QuotingEngine.QuotingEngine,
+    private _publisher: Publish.Publisher,
+    private _quoter: Quoter.Quoter,
+    private _broker: Broker.ExchangeBroker,
+    private _activeRepo: Active.ActiveRepository
+  ) {
+    _activeRepo.ExchangeConnectivity.on(this.sendQuote);
+    _quotingEngine.QuoteChanged.on(this.sendQuote);
+    _publisher.registerSnapshot(Models.Topics.QuoteStatus, () => this.latestStatus === null ? [] : [this.latestStatus]);
+  }
+
+  private checkCrossedQuotes = (side: Models.Side, px: number): boolean => {
+    var oppSide = side === Models.Side.Bid ? Models.Side.Ask : Models.Side.Bid;
+
+    var doesQuoteCross = oppSide === Models.Side.Bid
+      ? (a, b) => a.price >= b
+      : (a, b) => a.price <= b;
+
+    let qs = this._quotingEngine.latestQuote[oppSide === Models.Side.Bid ? 'bid' : 'ask'];
+    if (qs && doesQuoteCross(qs.price, px)) {
+      console.warn('quotesender', 'crossing quote detected! gen quote at', px, 'would crossed with', Models.Side[oppSide], 'quote at', qs);
+      return true;
+    }
+    return false;
+  };
+
+  private sendQuote = (): void => {
+    var quote = this._quotingEngine.latestQuote;
+
+    let askStatus = Models.QuoteStatus.Held;
+    let bidStatus = Models.QuoteStatus.Held;
+
+    if (quote !== null) {
+      if (this._activeRepo.latest) {
+        if (quote.ask !== null && (this._broker.hasSelfTradePrevention || !this.checkCrossedQuotes(Models.Side.Ask, quote.ask.price)))
+          askStatus = Models.QuoteStatus.Live;
+
+        if (quote.bid !== null && (this._broker.hasSelfTradePrevention || !this.checkCrossedQuotes(Models.Side.Bid, quote.bid.price)))
+          bidStatus = Models.QuoteStatus.Live;
+      }
+
+      if (askStatus === Models.QuoteStatus.Live)
+        this._quoter.updateQuote(new Models.Timestamped(quote.ask, this._timeProvider.utcNow()), Models.Side.Ask);
+      else
+        this._quoter.cancelQuote(new Models.Timestamped(Models.Side.Ask, this._timeProvider.utcNow()));
+
+      if (bidStatus === Models.QuoteStatus.Live)
+        this._quoter.updateQuote(new Models.Timestamped(quote.bid, this._timeProvider.utcNow()), Models.Side.Bid);
+      else
+        this._quoter.cancelQuote(new Models.Timestamped(Models.Side.Bid, this._timeProvider.utcNow()));
     }
 
-    constructor(
-            private _timeProvider: Utils.ITimeProvider,
-            private _quotingEngine: QuotingEngine.QuotingEngine,
-            private _publisher: Publish.Publisher,
-            private _quoter: Quoter.Quoter,
-            private _details: Broker.ExchangeBroker,
-            private _activeRepo: Active.ActiveRepository) {
-        _activeRepo.ExchangeConnectivity.on(this.sendQuote);
-        _quotingEngine.QuoteChanged.on(this.sendQuote);
-        _publisher.registerSnapshot(Models.Topics.QuoteStatus, () => this.latestStatus === null ? [] : [this.latestStatus]);
-    }
-
-    private checkCrossedQuotes = (side: Models.Side, px: number): boolean => {
-        var oppSide = side === Models.Side.Bid ? Models.Side.Ask : Models.Side.Bid;
-
-        var doesQuoteCross = oppSide === Models.Side.Bid
-            ? (a, b) => a.price >= b
-            : (a, b) => a.price <= b;
-
-        let qs = this._quotingEngine.latestQuote[oppSide === Models.Side.Bid ? 'bid' : 'ask'];
-        if (qs && doesQuoteCross(qs.price, px)) {
-            console.warn('quotesender', 'crossing quote detected! gen quote at', px, 'would crossed with', Models.Side[oppSide], 'quote at', qs);
-            return true;
-        }
-        return false;
-    };
-
-    private sendQuote = (): void => {
-        var quote = this._quotingEngine.latestQuote;
-
-        let askStatus = Models.QuoteStatus.Held;
-        let bidStatus = Models.QuoteStatus.Held;
-
-        if (quote !== null) {
-          if (this._activeRepo.latest) {
-            if (quote.ask !== null && (this._details.hasSelfTradePrevention || !this.checkCrossedQuotes(Models.Side.Ask, quote.ask.price)))
-              askStatus = Models.QuoteStatus.Live;
-
-            if (quote.bid !== null && (this._details.hasSelfTradePrevention || !this.checkCrossedQuotes(Models.Side.Bid, quote.bid.price)))
-              bidStatus = Models.QuoteStatus.Live;
-          }
-
-          if (askStatus === Models.QuoteStatus.Live)
-            this._quoter.updateQuote(new Models.Timestamped(quote.ask, this._timeProvider.utcNow()), Models.Side.Ask);
-          else
-            this._quoter.cancelQuote(new Models.Timestamped(Models.Side.Ask, this._timeProvider.utcNow()));
-
-          if (bidStatus === Models.QuoteStatus.Live)
-            this._quoter.updateQuote(new Models.Timestamped(quote.bid, this._timeProvider.utcNow()), Models.Side.Bid);
-          else
-            this._quoter.cancelQuote(new Models.Timestamped(Models.Side.Bid, this._timeProvider.utcNow()));
-        }
-
-        this.latestStatus = new Models.TwoSidedQuoteStatus(bidStatus, askStatus);
-    };
+    this.latestStatus = new Models.TwoSidedQuoteStatus(bidStatus, askStatus);
+  };
 }
--- src/server/quoter.ts
+++ src/server/quoter.ts
@@ -18,157 +18,135 @@ export class Quoter {
   }
 
   public updateQuote = (q: Models.Timestamped<Models.Quote>, side: Models.Side) => {
-    switch (side) {
-      case Models.Side.Ask:
-        this._askQuoter.updateQuote(q);
-      case Models.Side.Bid:
-        this._bidQuoter.updateQuote(q);
-    }
+    this[side === Models.Side.Ask ? '_askQuoter' : '_bidQuoter'].updateQuote(q);
   };
 
   public cancelQuote = (s: Models.Timestamped<Models.Side>) => {
-    switch (s.data) {
-      case Models.Side.Ask:
-        this._askQuoter.cancelQuote(s.time);
-      case Models.Side.Bid:
-        this._bidQuoter.cancelQuote(s.time);
-    }
+    this[s.data === Models.Side.Ask ? '_askQuoter' : '_bidQuoter'].cancelQuote(s.time);
   };
 }
 
 export class ExchangeQuoter {
-    private _exchange: Models.Exchange;
+  private _exchange: Models.Exchange;
 
-    constructor(
-      private _broker: Broker.ExchangeBroker,
-      private _orderBroker: Broker.OrderBroker,
-      private _side: Models.Side,
-      private _qlParamRepo: QuotingParameters.QuotingParametersRepository
-    ) {
-      this._exchange = _broker.exchange();
+  constructor(
+    private _broker: Broker.ExchangeBroker,
+    private _orderBroker: Broker.OrderBroker,
+    private _side: Models.Side,
+    private _qlParamRepo: QuotingParameters.QuotingParametersRepository
+  ) {
+    this._exchange = _broker.exchange();
+  }
+
+  private orderCacheSide = (sort?: boolean) => {
+    let orderSide = Array.from(this._orderBroker.orderCache.allOrders.values()).filter(x => this._side === x.side);
+    if (sort) {
+      if (this._side === Models.Side.Bid)
+        orderSide.sort(function(a,b){return a.price<b.price?1:(a.price>b.price?-1:0);});
+      else orderSide.sort(function(a,b){return a.price>b.price?1:(a.price<b.price?-1:0);});
+    }
+    return orderSide;
+  };
+
+  public updateQuote = (q: Models.Timestamped<Models.Quote>) => {
+    if (this._broker.connectStatus !== Models.ConnectivityStatus.Connected)
+      return;
+
+    const orderSide = this.orderCacheSide();
+    const dupe = orderSide.filter(x => q.data.price.toFixed(8) === x.price.toFixed(8)).length;
+
+    if (this._qlParamRepo.latest.mode !== Models.QuotingMode.AK47) {
+      if (orderSide.length) {
+        if (!dupe) this.modify(q);
+      } else this.start(q);
+      return;
     }
 
-    public updateQuote = (q: Models.Timestamped<Models.Quote>) => {
-      if (this._broker.connectStatus !== Models.ConnectivityStatus.Connected)
-        return;
+    if (!dupe && orderSide.length >= this._qlParamRepo.latest.bullets) {
+      this.modify(q);
+      return;
+    }
 
-      let dupePrice = 0;
-      let dupeSide = 0;
-      this._orderBroker.orderCache.allOrders.forEach(x => {
-        if (this._side === x.side) {
-          dupeSide++;
-          if (q.data.price === x.price) dupePrice++;
-        }
-      });
+    this.start(q);
+  };
 
-      if (this._qlParamRepo.latest.mode !== Models.QuotingMode.AK47) {
-        if (!dupeSide) this.start(q);
-        else if (!dupePrice) this.modify(q);
-        return;
-      }
+  private cancelWorstQuotes = (price: number, time: Date) => {
+    this.orderCacheSide().filter(x =>
+      this._side === Models.Side.Bid
+        ? price < x.price
+        : price > x.price
+    ).forEach(x =>
+      this._orderBroker.cancelOrder(new Models.OrderCancel(x.orderId, this._exchange, time))
+    );
+  };
 
-      if (dupeSide >= this._qlParamRepo.latest.bullets && !dupePrice) {
-        this.modify(q);
-        return;
-      }
+  public cancelQuote = (t: Date) => {
+    if (this._broker.connectStatus !== Models.ConnectivityStatus.Connected) return;
+    this.stop(t);
+  };
 
-      this.start(q);
-    };
+  private modify = (q: Models.Timestamped<Models.Quote>) => {
+    if (this._qlParamRepo.latest.mode === Models.QuotingMode.AK47) this.stopWorst(q.time);
+    else this.stop(q.time);
+    this.start(q);
+  };
 
-    private cancelWorstQuotes = (price: number, time: Date) => {
-      this._orderBroker.orderCache.allOrders.forEach(x => {
-        if (this._side === x.side && ((this._side === Models.Side.Bid && price < x.price) || price > x.price))
-          this._orderBroker.cancelOrder(new Models.OrderCancel(x.orderId, this._exchange, time));
-      });
-    };
+  private start = (q: Models.Timestamped<Models.Quote>) => {
+    let price: number = q.data.price;
+    let orderSide = this.orderCacheSide(true);
+    if (orderSide.filter(x => price.toFixed(8) == x.price.toFixed(8)
+      || (this._qlParamRepo.latest.mode === Models.QuotingMode.AK47 &&
+           ((price + (this._qlParamRepo.latest.range - 1e-2)) >= x.price
+           && (price - (this._qlParamRepo.latest.range - 1e-2)) <= x.price))
+    ).length) {
+      if (this._qlParamRepo.latest.mode === Models.QuotingMode.AK47) {
+        if (orderSide.length<this._qlParamRepo.latest.bullets) {
+          let incPrice: number = (this._qlParamRepo.latest.range * (this._side === Models.Side.Bid ? -1 : 1 ));
+          let oldPrice:number = 0;
+          let len:number  = 0;
+          orderSide.forEach(x => {
+            if (oldPrice>0 && (this._side === Models.Side.Bid?x.price<price:x.price>price)) {
+              price = oldPrice + incPrice;
+              if (Math.abs(x.price - oldPrice)>incPrice) return false;
+            }
+            oldPrice = x.price;
+            ++len;
+          });
+          if (len==orderSide.length) price = orderSide.slice(-1).pop().price + incPrice;
+          if (orderSide.filter(x =>
+            (price + (this._qlParamRepo.latest.range - 1e-2)) >= x.price
+            && (price - (this._qlParamRepo.latest.range - 1e-2)) <= x.price
+          ).length) return;
+          this.cancelWorstQuotes(q.data.price, q.time);
+          price = Utils.roundNearest(price, this._broker.minTickIncrement);
+          q.data.price = price;
+        } else return;
+      } else return;
+    }
 
-    public cancelQuote = (t: Date) => {
-      if (this._broker.connectStatus !== Models.ConnectivityStatus.Connected) return;
-      this.stop(t);
-    };
+    this._orderBroker.sendOrder(new Models.SubmitNewOrder(
+      this._side,
+      q.data.size,
+      Models.OrderType.Limit,
+      price,
+      Models.TimeInForce.GTC,
+      q.data.isPong,
+      this._exchange,
+      q.time,
+      true,
+      Models.OrderSource.Quote
+    ));
+  };
 
-    private modify = (q: Models.Timestamped<Models.Quote>) => {
-      if (this._qlParamRepo.latest.mode === Models.QuotingMode.AK47) this.stopWorst(q.time);
-      else this.stop(q.time);
-      this.start(q);
-    };
+  private stopWorst = (t: Date) => {
+    const orderSide = this.orderCacheSide(true);
+    if (orderSide.length)
+      this._orderBroker.cancelOrder(new Models.OrderCancel(orderSide.shift().orderId, this._exchange, t));
+  };
 
-    private start = (q: Models.Timestamped<Models.Quote>) => {
-        let price: number = q.data.price;
-        let dupePrice = 0;
-        let dupePriceAK47 = 0;
-        let dupeSide = 0;
-        let bestPrice = null;
-        this._orderBroker.orderCache.allOrders.forEach(x => {
-          if (this._side === x.side) {
-            dupeSide++;
-            if (price === x.price) dupePrice++;
-            if (this._qlParamRepo.latest.mode === Models.QuotingMode.AK47
-              && ((price + (this._qlParamRepo.latest.range - 1e-2)) >= x.price
-                && (price - (this._qlParamRepo.latest.range - 1e-2)) <= x.price)) dupePriceAK47++;
-            if (bestPrice === null || ((this._side === Models.Side.Bid && bestPrice.price > x.price) || bestPrice.price < x.price))
-              bestPrice = x;
-          }
-        });
-        if (dupePrice || dupePriceAK47) {
-          if (this._qlParamRepo.latest.mode === Models.QuotingMode.AK47) {
-            if (dupeSide<this._qlParamRepo.latest.bullets) {
-              let incPrice: number = (this._qlParamRepo.latest.range * (this._side === Models.Side.Bid ? -1 : 1 ));
-              let oldPrice:number = 0;
-              let len:number  = 0;
-              this._orderBroker.orderCache.allOrders.forEach(x => {
-                if (this._side === x.side) {
-                  if (oldPrice>0 && (this._side === Models.Side.Bid?x.price<price:x.price>price)) {
-                    price = oldPrice + incPrice;
-                    if (Math.abs(x.price - oldPrice)>incPrice) return false;
-                  }
-                  oldPrice = x.price;
-                  ++len;
-                }
-              });
-              if (len==dupeSide && bestPrice) price = bestPrice.price + incPrice;
-              dupePriceAK47 = 0;
-              this._orderBroker.orderCache.allOrders.forEach(x => {
-                if (this._side === x.side && this._qlParamRepo.latest.mode === Models.QuotingMode.AK47
-                  && ((price + (this._qlParamRepo.latest.range - 1e-2)) >= x.price
-                    && (price - (this._qlParamRepo.latest.range - 1e-2)) <= x.price)) dupePriceAK47++;
-              });
-              if (dupePriceAK47) return;
-              this.cancelWorstQuotes(q.data.price, q.time);
-              price = Utils.roundNearest(price, this._broker.minTickIncrement);
-              q.data.price = price;
-            } else return;
-          } else return;
-        }
-
-        this._orderBroker.sendOrder(new Models.SubmitNewOrder(
-          this._side,
-          q.data.size,
-          Models.OrderType.Limit,
-          price,
-          Models.TimeInForce.GTC,
-          q.data.isPong,
-          this._exchange,
-          q.time,
-          true,
-          Models.OrderSource.Quote
-        ));
-    };
-
-    private stopWorst = (t: Date) => {
-      let worst = null;
-      this._orderBroker.orderCache.allOrders.forEach(x => {
-        if (this._side === x.side && (worst === null || ((this._side === Models.Side.Bid && worst.price < x.price) || worst.price > x.price)))
-          worst = x;
-      });
-      if (worst)
-        this._orderBroker.cancelOrder(new Models.OrderCancel(worst.orderId, this._exchange, t));
-    };
-
-    private stop = (t: Date) => {
-      this._orderBroker.orderCache.allOrders.forEach(x => {
-        if (this._side === x.side)
-          this._orderBroker.cancelOrder(new Models.OrderCancel(x.orderId, this._exchange, t));
-      });
-    };
+  private stop = (t: Date) => {
+    this.orderCacheSide().forEach(x =>
+      this._orderBroker.cancelOrder(new Models.OrderCancel(x.orderId, this._exchange, t))
+    );
+  };
 }
