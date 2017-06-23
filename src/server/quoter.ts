ff80151ad4aae18cd7757b6972fc2815bfcc1953
Carles Tubio <ctubio@users.noreply.github.com>
2017-06-23 18:26:56 +0000

    Updated quoter without spawn multiple classes.
    Free Software Free Society
    
    To support commits by ctubio,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/server/quoter.ts
+++ src/server/quoter.ts
@@ -5,77 +5,59 @@ import QuotingParameters = require("./quoting-parameters");
 import moment = require("moment");
 
 export class Quoter {
-  private _bidQuoter: ExchangeQuoter;
-  private _askQuoter: ExchangeQuoter;
-
-  constructor(
-    broker: Broker.ExchangeBroker,
-    orderBroker: Broker.OrderBroker,
-    private _qlParamRepo: QuotingParameters.QuotingParametersRepository
-  ) {
-    this._bidQuoter = new ExchangeQuoter(broker, orderBroker, Models.Side.Bid, this._qlParamRepo);
-    this._askQuoter = new ExchangeQuoter(broker, orderBroker, Models.Side.Ask, this._qlParamRepo);
-  }
-
-  public updateQuote = (q: Models.Timestamped<Models.Quote>, side: Models.Side) => {
-    this[side === Models.Side.Ask ? '_askQuoter' : '_bidQuoter'].updateQuote(q);
-  };
-
-  public cancelQuote = (s: Models.Timestamped<Models.Side>) => {
-    this[s.data === Models.Side.Ask ? '_askQuoter' : '_bidQuoter'].cancelQuote(s.time);
-  };
-}
-
-class ExchangeQuoter {
   private _exchange: Models.Exchange;
 
   constructor(
     private _broker: Broker.ExchangeBroker,
     private _orderBroker: Broker.OrderBroker,
-    private _side: Models.Side,
     private _qlParamRepo: QuotingParameters.QuotingParametersRepository
   ) {
     this._exchange = _broker.exchange();
   }
 
-  private orderCacheSide = (sort?: boolean) => {
+  public updateQuote = (q: Models.Timestamped<Models.Quote>, side: Models.Side) => {
+    if (this._broker.connectStatus !== Models.ConnectivityStatus.Connected)
+      return;
+
+    const orderSide = this.orderCacheSide(side, false);
+    const dupe = orderSide.filter(x => q.data.price.toFixed(8) === x.price.toFixed(8)).length;
+
+    if (this._qlParamRepo.latest.mode !== Models.QuotingMode.AK47) {
+      if (orderSide.length) {
+        if (!dupe) this.modify(side, q);
+      } else this.start(side, q);
+      return;
+    }
+
+    if (!dupe && orderSide.length >= this._qlParamRepo.latest.bullets) {
+      this.modify(side, q);
+      return;
+    }
+
+    this.start(side, q);
+  };
+
+  public cancelQuote = (s: Models.Timestamped<Models.Side>) => {
+    if (this._broker.connectStatus !== Models.ConnectivityStatus.Connected) return;
+    this.stop(s.data, s.time);
+  };
+
+  private orderCacheSide = (side: Models.Side, sort: boolean) => {
     let orderSide = [];
     this._orderBroker.orderCache.allOrders.forEach(x => {
-      if (this._side === x.side) orderSide.push(x);
+      if (side === x.side) orderSide.push(x);
     });
     if (sort) {
-      if (this._side === Models.Side.Bid)
+      if (side === Models.Side.Bid)
         orderSide.sort(function(a,b){return a.price<b.price?1:(a.price>b.price?-1:0);});
       else orderSide.sort(function(a,b){return a.price>b.price?1:(a.price<b.price?-1:0);});
     }
     return orderSide;
   };
 
-  public updateQuote = (q: Models.Timestamped<Models.Quote>) => {
-    if (this._broker.connectStatus !== Models.ConnectivityStatus.Connected)
-      return;
-
-    const orderSide = this.orderCacheSide();
-    const dupe = orderSide.filter(x => q.data.price.toFixed(8) === x.price.toFixed(8)).length;
-
-    if (this._qlParamRepo.latest.mode !== Models.QuotingMode.AK47) {
-      if (orderSide.length) {
-        if (!dupe) this.modify(q);
-      } else this.start(q);
-      return;
-    }
-
-    if (!dupe && orderSide.length >= this._qlParamRepo.latest.bullets) {
-      this.modify(q);
-      return;
-    }
-
-    this.start(q);
-  };
-
-  private cancelWorstQuotes = (price: number, time: Date) => {
-    this.orderCacheSide().filter(x =>
-      this._side === Models.Side.Bid
+  private cancelWorstQuotes = (side: Models.Side, price: number, time: Date) => {
+    this.orderCacheSide(side, false).filter(x =>
+      side === Models.Side.Bid
         ? price < x.price
         : price > x.price
     ).forEach(x =>
@@ -83,20 +65,15 @@ class ExchangeQuoter {
     );
   };
 
-  public cancelQuote = (t: Date) => {
-    if (this._broker.connectStatus !== Models.ConnectivityStatus.Connected) return;
-    this.stop(t);
+  private modify = (side: Models.Side, q: Models.Timestamped<Models.Quote>) => {
+    if (this._qlParamRepo.latest.mode === Models.QuotingMode.AK47) this.stopWorst(side, q.time);
+    else this.stop(side, q.time);
+    this.start(side, q);
   };
 
-  private modify = (q: Models.Timestamped<Models.Quote>) => {
-    if (this._qlParamRepo.latest.mode === Models.QuotingMode.AK47) this.stopWorst(q.time);
-    else this.stop(q.time);
-    this.start(q);
-  };
-
-  private start = (q: Models.Timestamped<Models.Quote>) => {
+  private start = (side: Models.Side, q: Models.Timestamped<Models.Quote>) => {
     let price: number = q.data.price;
-    let orderSide = this.orderCacheSide(true);
+    let orderSide = this.orderCacheSide(side, true);
     if (orderSide.filter(x => price.toFixed(8) == x.price.toFixed(8)
       || (this._qlParamRepo.latest.mode === Models.QuotingMode.AK47 &&
            ((price + (this._qlParamRepo.latest.range - 1e-2)) >= x.price
@@ -104,11 +81,11 @@ class ExchangeQuoter {
     ).length) {
       if (this._qlParamRepo.latest.mode === Models.QuotingMode.AK47) {
         if (orderSide.length<this._qlParamRepo.latest.bullets) {
-          let incPrice: number = (this._qlParamRepo.latest.range * (this._side === Models.Side.Bid ? -1 : 1 ));
+          let incPrice: number = (this._qlParamRepo.latest.range * (side === Models.Side.Bid ? -1 : 1 ));
           let oldPrice:number = 0;
           let len:number  = 0;
           orderSide.forEach(x => {
-            if (oldPrice>0 && (this._side === Models.Side.Bid?x.price<price:x.price>price)) {
+            if (oldPrice>0 && (side === Models.Side.Bid?x.price<price:x.price>price)) {
               price = oldPrice + incPrice;
               if (Math.abs(x.price - oldPrice)>incPrice) return false;
             }
@@ -120,7 +97,7 @@ class ExchangeQuoter {
             (price + (this._qlParamRepo.latest.range - 1e-2)) >= x.price
             && (price - (this._qlParamRepo.latest.range - 1e-2)) <= x.price
           ).length) return;
-          this.cancelWorstQuotes(q.data.price, q.time);
+          this.cancelWorstQuotes(side, q.data.price, q.time);
           price = Utils.roundNearest(price, this._broker.minTickIncrement);
           q.data.price = price;
         } else return;
@@ -128,7 +105,7 @@ class ExchangeQuoter {
     }
 
     this._orderBroker.sendOrder(new Models.SubmitNewOrder(
-      this._side,
+      side,
       q.data.size,
       Models.OrderType.Limit,
       price,
@@ -141,14 +118,14 @@ class ExchangeQuoter {
     ));
   };
 
-  private stopWorst = (t: Date) => {
-    const orderSide = this.orderCacheSide(true);
+  private stopWorst = (side: Models.Side, t: Date) => {
+    const orderSide = this.orderCacheSide(side, true);
     if (orderSide.length)
       this._orderBroker.cancelOrder(new Models.OrderCancel(orderSide.shift().orderId, this._exchange, t));
   };
 
-  private stop = (t: Date) => {
-    this.orderCacheSide().forEach(x =>
+  private stop = (side: Models.Side, t: Date) => {
+    this.orderCacheSide(side, false).forEach(x =>
       this._orderBroker.cancelOrder(new Models.OrderCancel(x.orderId, this._exchange, t))
     );
   };
