ee3e578de2e6f57734b09a77c5609080d4550c84
Carles Tubio <ctubio@users.noreply.github.com>
2017-07-08 01:33:11 +0000

    Added HTTPS and dummy websocket handshake.
    Free Software Free Society
    
    To support commits by ctubio,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/lib/ui.h
+++ src/lib/ui.h
@@ -49,16 +49,43 @@ namespace K {
         Isolate* isolate = Isolate::GetCurrent();
         group = hub.createGroup<uWS::SERVER>(uWS::PERMESSAGE_DEFLATE);
         group->setUserData(new GroupData);
-        group->onHttpRequest([&isolate](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t length, size_t remainingBytes) {
+        GroupData *groupData = (GroupData *) group->getUserData();
+group->onConnection([isolate, groupData](uWS::WebSocket<uWS::SERVER> *webSocket, uWS::HttpRequest req) {
+cout << "onConnectionnn " << endl;
+    groupData->size++;
+    // HandleScope hs(isolate);
+    // HandleScope hs(isolate);
+    // Local<Value> argv[] = {wrapSocket(webSocket, isolate)};
+});
+hub.onHttpUpgrade([](uWS::HttpSocket<uWS::SERVER> *s, uWS::HttpRequest req) {
+cout << "onHttpUpgrade " << endl;
+});
+        group->onHttpRequest([isolate](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t length, size_t remainingBytes) {
           if (req.getMethod() == uWS::HttpMethod::METHOD_GET) {
             string url;
+            stringstream content;
             string document = "HTTP/1.1 200 OK\r\nConnection: keep-alive\r\nAccept-Ranges: bytes\r\nVary: Accept-Encoding\r\n";
             string path = req.getUrl().toString();
             string::size_type n = 0;
             while ((n = path.find("..", n)) != string::npos) path.replace(n, 2, "");
             if (path.substr(1, path.find_last_of('/')-1) == "socket.io") {
               cout << "path " << path << endl;
-              res->end(nullptr, 0);
+              std::cout << to_string(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count()) << endl;
+              // cout << "key " <<  req.getHeader("sec-websocket-key").toString() << endl;
+              // cout << "extensions " <<  req.getHeader("sec-websocket-extensions").toString() << endl;
+              // cout << "protocol " <<  req.getHeader("sec-websocket-protocol").toString() << endl;
+              // res->getHttpSocket()->upgrade(
+                // req.getHeader("sec-websocket-key").toString().data(),
+                // req.getHeader("sec-websocket-extensions").toString().data(),
+                // req.getHeader("sec-websocket-extensions").toString().length(),
+                // req.getHeader("sec-websocket-protocol").toString().data(),
+                // req.getHeader("sec-websocket-protocol").toString().length(),
+                // (bool*)new bool(true)
+              // );
+              content << "90:0{\"sid\":\"" << to_string(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count()) << "\",\"upgrades\":[\"websocket\"],\"pingInterval\":25000,\"pingTimeout\":60000}2:40";
+              document.append("Content-Type: text/plain; charset=UTF-8\r\n");
+              document.append("Content-Length: ").append(to_string(content.str().length())).append("\r\n\r\n").append(content.str());
+              res->write(document.data(), document.length());
             } else {
               const string leaf = path.substr(path.find_last_of('.')+1);
               if (leaf == "/") {
@@ -74,7 +101,6 @@ namespace K {
                 document.append("Content-Type: image/png\r\n");
                 url = path;
               }
-              stringstream content;
               if (!url.length()) {
                 document = "HTTP/1.1 404 Not Found\r\n";
                 content << "Today, is a beautiful day.";
@@ -87,7 +113,11 @@ namespace K {
             }
           }
         });
-        cout << "listen: " << (hub.listen(port, nullptr, 0, group) ? "yes" : "no") << " on port " << to_string(port) << endl;
+        uS::TLS::Context c = uS::TLS::createContext("dist/sslcert/server.crt", "dist/sslcert/server.key", "");
+        if (hub.listen(port, c, 0, group))
+          cout << "listen: " << "HTTPS" << " on port " << to_string(port) << endl;
+        else if (hub.listen(port, nullptr, 0, group))
+          cout << "listen: " << "HTTP" << " on port " << to_string(port) << endl;
       }
       ~UI() {
         delete group;
@@ -99,6 +129,7 @@ namespace K {
         Persistent<Function> *messageCallback = &groupData->messageHandler;
         messageCallback->Reset(isolate, Local<Function>::Cast(args[0]));
         uws->group->onMessage([isolate, messageCallback](uWS::WebSocket<uWS::SERVER> *webSocket, const char *message, size_t length, uWS::OpCode opCode) {
+cout << "msgss" << endl;
           HandleScope hs(isolate);
           Local<Value> argv[] = {wrapMessage(message, length, opCode, isolate), getDataV8(webSocket, isolate)};
           Local<Function>::New(isolate, *messageCallback)->Call(isolate->GetCurrentContext()->Global(), 2, argv);
