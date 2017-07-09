3a4c52f2c8b80d9d2ea43bead4cec9c49a67993c
Carles Tubio <ctubio@users.noreply.github.com>
2017-07-09 22:05:09 +0000

    Fixed return of boolean values.
    Free Software Free Society
    
    To support commits by ctubio,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/lib/ui.h
+++ src/lib/ui.h
@@ -110,8 +110,10 @@ namespace K {
           if (length > 1 && (session->cb.find(string(message).substr(0,2)) != session->cb.end())) {
             JSON Json;
             HandleScope hs(isolate);
-            MaybeLocal<Value> array = (length > 2 && (message[2] == '[' || message[2] == '{')) ? Json.Parse(isolate->GetCurrentContext(), String::NewFromUtf8(isolate, string(message).substr(2, length-2).data())) : String::NewFromUtf8(isolate, length > 2 ? string(message).substr(2, length-2).data() : "");
-            Local<Value> argv[] = {String::NewFromUtf8(isolate, ""), array.IsEmpty() ? (Local<Value>)Array::New(isolate) : array.ToLocalChecked()};
+            MaybeLocal<Value> v = (length > 2 && (message[2] == '[' || message[2] == '{'))
+              ? Json.Parse(isolate->GetCurrentContext(), String::NewFromUtf8(isolate, string(message).substr(2, length-2).data()))
+              : String::NewFromUtf8(isolate, length > 2 ? string(message).substr(2, length-2).data() : "");
+            Local<Value> argv[] = {String::NewFromUtf8(isolate, ""), (string(message).substr(2, length-2) == "true" || string(message).substr(2, length-2) == "false") ? (Local<Value>)Boolean::New(isolate, string(message).substr(2, length-2) == "true") : (v.IsEmpty() ? (Local<Value>)String::Empty(isolate) : v.ToLocalChecked())};
             Local<Value> reply = Local<Function>::New(isolate, session->cb[string(message).substr(0,2)])->Call(isolate->GetCurrentContext()->Global(), 2, argv);
             if (!reply->IsUndefined()) webSocket->send(string("=").append(string(message).substr(1,1)).append(*String::Utf8Value(Json.Stringify(isolate->GetCurrentContext(), reply->ToObject()).ToLocalChecked())).data(), uWS::OpCode::TEXT);
           }
