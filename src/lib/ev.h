c89d95b2c0da93b5835dc3fdd5885a9976e258dc
Carles Tubio <ctubio@users.noreply.github.com>
2017-07-15 02:07:17 +0000

    Updated events lib.
    Free Software Free Society
    
    To support commits by ctubio,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/lib/ev.h
+++ src/lib/ev.h
@@ -2,31 +2,38 @@
 #define K_EV_H_
 
 namespace K {
-  struct Ev { map<string, vector<CopyablePersistentTraits<Function>::CopyablePersistent>> cb; } ev;
+  typedef void (*evCb)(string);
+  struct Ev { map<string, vector<CopyablePersistentTraits<Function>::CopyablePersistent>> _cb; map<string, vector<evCb>> cb; } ev;
   class EV {
     public:
       static void main(Local<Object> exports) {
         Ev *ev = new Ev;
-        NODE_SET_METHOD(exports, "evOn", EV::evOn);
-        NODE_SET_METHOD(exports, "evUp", EV::evUp);
+        NODE_SET_METHOD(exports, "evOn", EV::_evOn);
+        NODE_SET_METHOD(exports, "evUp", EV::_evUp);
       }
-    private:
-      static void evOn(const FunctionCallbackInfo<Value> &args) {
+      static void evUp(string k, Local<Object> o) {
         Isolate* isolate = args.GetIsolate();
         HandleScope scope(isolate);
-        string k = string(*String::Utf8Value(args[0]->ToString()));
-        Persistent<Function> cb;
-        cb.Reset(isolate, Local<Function>::Cast(args[1]));
-        ev.cb[k].push_back(cb);
-      };
-      static void evUp(const FunctionCallbackInfo<Value> &args) {
-        Isolate* isolate = args.GetIsolate();
-        HandleScope scope(isolate);
-        string k = string(*String::Utf8Value(args[0]->ToString()));
         if (ev.cb.find(k) == ev.cb.end()) return;
+        for (vector<evCb>::iterator cb = ev.cb[k].begin(); cb != ev.cb[k].end(); ++cb)
+          (*cb)(o);  
+      };
+      static void _evOn(const FunctionCallbackInfo<Value> &args) {
+        Isolate* isolate = args.GetIsolate();
+        HandleScope scope(isolate);
+        string k = string(*String::Utf8Value(args[0]->ToString()));
+        Persistent<Function> _cb;
+        _cb.Reset(isolate, Local<Function>::Cast(args[1]));
+        ev._cb[k].push_back(_cb);
+      };
+      static void _evUp(const FunctionCallbackInfo<Value> &args) {
+        Isolate* isolate = args.GetIsolate();
+        HandleScope scope(isolate);
+        string k = string(*String::Utf8Value(args[0]->ToString()));
+        if (ev._cb.find(k) == ev._cb.end()) return;
         Local<Value> argv[] = {args[1]};
-        for (vector<CopyablePersistentTraits<Function>::CopyablePersistent>::iterator cb = ev.cb[k].begin(); cb != ev.cb[k].end(); ++cb)
-          Local<Function>::New(isolate, *cb)->Call(isolate->GetCurrentContext()->Global(), 1, argv);
+        for (vector<CopyablePersistentTraits<Function>::CopyablePersistent>::iterator _cb = ev._cb[k].begin(); _cb != ev._cb[k].end(); ++_cb)
+          Local<Function>::New(isolate, *_cb)->Call(isolate->GetCurrentContext()->Global(), 1, argv);
       };
   };
 }
