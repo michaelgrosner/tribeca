3d1ff0709062e81ddfcfeac990dff525bd159f25
Carles Tubio <ctubio@users.noreply.github.com>
2017-07-15 02:08:03 +0000

    Updated DB lib.
    Free Software Free Society
    
    To support commits by ctubio,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/lib/db.h
+++ src/lib/db.h
@@ -9,43 +9,20 @@ namespace K {
     public:
       static void main(Local<Object> exports) {
         Isolate* isolate = exports->GetIsolate();
+        dbFpath = string("/data/db/K.").append(to_string((int)CF::cfExchange())).append(".").append(to_string((int)CF::cfBase())).append(".").append(to_string((int)CF::cfQuote())).append(".db");
+        if (sqlite3_open(dbFpath.data(), &db)) { cout << sqlite3_errmsg(db) << endl; exit(1); }
+        cout << "DB " << dbFpath << " loaded OK" << endl;
         Local<FunctionTemplate> o = FunctionTemplate::New(isolate, NEw);
         o->InstanceTemplate()->SetInternalFieldCount(1);
-        o->SetClassName(String::NewFromUtf8(isolate, "DB"));
-        NODE_SET_PROTOTYPE_METHOD(o, "load", load);
-        NODE_SET_PROTOTYPE_METHOD(o, "insert", insert);
+        o->SetClassName(FN::v8S("DB"));
+        NODE_SET_PROTOTYPE_METHOD(o, "load", _load);
+        NODE_SET_PROTOTYPE_METHOD(o, "insert", _insert);
         sqlite_.Reset(isolate, o->GetFunction());
-        exports->Set(String::NewFromUtf8(isolate, "DB"), o->GetFunction());
+        exports->Set(FN::v8S("DB"), o->GetFunction());
         NODE_SET_METHOD(exports, "dbSize", DB::_dbSize);
       }
-    protected:
-      int exchange;
-      int base;
-      int quote;
-    private:
-      explicit DB(int e_, int b_, int q_): exchange(e_), base(b_), quote(q_) {
-        Isolate* isolate = Isolate::GetCurrent();
-        dbFpath = string("/data/db/K.").append(to_string(exchange)).append(".").append(to_string(base)).append(".").append(to_string(quote)).append(".db");
-        if (sqlite3_open(dbFpath.data(), &db)) isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, sqlite3_errmsg(db))));
-        cout << "DB " << dbFpath << " loaded OK" << endl;
-      }
-      ~DB() {
-        sqlite3_close(db);
-      }
-      static void NEw(const FunctionCallbackInfo<Value>& args) {
-        Isolate* isolate = args.GetIsolate();
-        HandleScope scope(isolate);
-        if (!args.IsConstructCall())
-          return (void)isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Use the 'new' operator to create new SQLite objects")));
-        DB* sqlite = new DB(args[0]->NumberValue(), args[1]->NumberValue(), args[2]->NumberValue());
-        sqlite->Wrap(args.This());
-        args.GetReturnValue().Set(args.This());
-      }
-      static void load(const FunctionCallbackInfo<Value>& args) {
-        Isolate* isolate = args.GetIsolate();
-        HandleScope scope(isolate);
+      static Local<Value> load(Isolate* isolate, string table) {
         char* zErrMsg = 0;
-        std::string table = string(*String::Utf8Value(args[0]->ToString()));
         sqlite3_exec(db,
           string("CREATE TABLE ").append(table).append("("                                                        \
             "id    INTEGER  PRIMARY KEY  AUTOINCREMENT        NOT NULL,"                                          \
@@ -62,25 +39,39 @@ namespace K {
         sqlite3_free(zErrMsg);
         if (json[strlen(json.data()) - 1] == ',') json.pop_back();
         JSON Json;
-        MaybeLocal<Value> array = Json.Parse(isolate->GetCurrentContext(), String::NewFromUtf8(isolate, json.append("]").data()));
-        if (array.IsEmpty()) args.GetReturnValue().Set(Array::New(isolate));
-        else args.GetReturnValue().Set(array.ToLocalChecked());
+        MaybeLocal<Value> array = Json.Parse(isolate->GetCurrentContext(), FN::v8S(json.append("]").data()));
+        return array.IsEmpty() ? (Local<Value>)Array::New(isolate) : array.ToLocalChecked();
+      }
+    protected:
+      int exchange;
+      int base;
+      int quote;
+    private:
+      explicit DB(int e_, int b_, int q_): exchange(e_), base(b_), quote(q_) {}
+      static void NEw(const FunctionCallbackInfo<Value>& args) {
+        Isolate* isolate = args.GetIsolate();
+        HandleScope scope(isolate);
+        if (!args.IsConstructCall())
+          return (void)isolate->ThrowException(Exception::TypeError(FN::v8S("Use the 'new' operator to create new SQLite objects")));
+        DB* sqlite = new DB(args[0]->NumberValue(), args[1]->NumberValue(), args[2]->NumberValue());
+        sqlite->Wrap(args.This());
+        args.GetReturnValue().Set(args.This());
+      }
+      static void _load(const FunctionCallbackInfo<Value>& args) {
+        Isolate* isolate = args.GetIsolate();
+        HandleScope scope(isolate);
+        args.GetReturnValue().Set(load(isolate, string(*String::Utf8Value(args[0]->ToString()))));
       }
       static int cb(void *param, int argc, char **argv, char **azColName) {
         string* json = reinterpret_cast<string*>(param);
         for (int i=0; i<argc; i++) json->append(argv[i]).append(",");
         return 0;
       }
-      static void insert(const FunctionCallbackInfo<Value>& args) {
-        Isolate* isolate = args.GetIsolate();
-        HandleScope scope(isolate);
+      static void insert(string table, Local<Object> o, bool rm = true, string id = "NULL", long time = 0) {
+        Isolate* isolate = Isolate::GetCurrent();
         char* zErrMsg = 0;
-        string table = string(*String::Utf8Value(args[0]->ToString()));
         JSON Json;
-        MaybeLocal<String> row = args[1]->IsUndefined() ? String::NewFromUtf8(isolate, "") : Json.Stringify(isolate->GetCurrentContext(), args[1]->ToObject());
-        bool rm = args[2]->IsUndefined() ? true : args[2]->BooleanValue();
-        string id = string(args[3]->IsUndefined() ? "NULL" : *String::Utf8Value(args[3]->ToString()));
-        long time = args[4]->IsUndefined() ? 0 : args[4]->NumberValue();
+        MaybeLocal<String> row = args[1]->IsUndefined() ? FN::v8S("") : Json.Stringify(isolate->GetCurrentContext(), o);
         sqlite3_exec(db,
           string((rm || id != "NULL" || time) ? string("DELETE FROM ").append(table)
           .append(id != "NULL" ? string(" WHERE id = ").append(id).append(";") : (
@@ -92,6 +83,11 @@ namespace K {
         );
         if (zErrMsg) printf("sqlite error: %s\n", zErrMsg);
         sqlite3_free(zErrMsg);
+      }
+      static void _insert(const FunctionCallbackInfo<Value>& args) {
+        Isolate* isolate = args.GetIsolate();
+        HandleScope scope(isolate);
+        insert(FN::S8v(args[0]->ToString()), args[1]->ToObject(), args[2]->IsUndefined() ? true : args[2]->BooleanValue(), string(args[3]->IsUndefined() ? "NULL" : *String::Utf8Value(args[3]->ToString())), args[4]->IsUndefined() ? 0 : args[4]->NumberValue());
       }
       static void _dbSize(const FunctionCallbackInfo<Value>& args) {
         Isolate* isolate = args.GetIsolate();
