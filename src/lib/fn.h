983cb6ced2cf73ea203209866c5b07256112c43e
Carles Tubio <ctubio@users.noreply.github.com>
2017-07-16 03:26:44 +0000

    Updated FN without useless var.
    Free Software Free Society
    
    To support commits by ctubio,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/lib/fn.h
+++ src/lib/fn.h
@@ -5,8 +5,7 @@ namespace K {
   class FN {
     public:
       static Local<String> v8S(string k) {
-        Isolate* isolate = Isolate::GetCurrent();
-        return String::NewFromUtf8(isolate, k.data());
+        return String::NewFromUtf8(Isolate::GetCurrent(), k.data());
       }
       static string S8v(Local<String> k) {
         return string(*String::Utf8Value(k));
