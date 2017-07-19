161700629cc53a6afafccec66f0182ecb0f2cc96
Carles Tubio <ctubio@users.noreply.github.com>
2017-07-19 02:23:09 +0000

    Added support button.
    Free Software Free Society
    
    To support commits by ctubio,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/client/main.ts
+++ src/client/main.ts
@@ -316,6 +316,7 @@ class DisplayOrder {
                                                     Not Connected
                                                 </span>
                                             </th>
+                                            <th style="width:121px;"></th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -417,6 +418,9 @@ class DisplayOrder {
                                                     [disabled]="!pair.quotingParameters.connected"
                                                     (click)="pair.quotingParameters.submit()"
                                                     value="Save" />
+                                            </td>
+                                            <td style="text-align: right;border-bottom: 3px solid #A0A0A0;">
+                                                <div id="coinwidget-bitcoin-1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u"></div>
                                             </td>
                                         </tr>
                                     </tbody>
@@ -760,6 +764,33 @@ class ClientComponent implements OnInit {
     this.homepage = pa.homepage;
     this.product.fixed = Math.max(0, Math.floor(Math.log10(pa.minTick)) * -1);
     setTimeout(this.resizeMatryoshka, 5000);
+    if (!(<any>window).CoinWidgetCom) (function(d, script) {
+        script = d.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.onload = () => {
+          (<any>window).CoinWidgetCom.go({
+            wallet_address: '1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u',
+            currency: 'bitcoin',
+            counter: 'count',
+            lbl_button: 'Support',
+            lbl_count: 'Donations',
+            lbl_amount: 'BTC',
+            lbl_address: 'Support the PUBLIC development of K.js',
+            qrcode: true,
+            alignment: 'bl',
+            decimals: 8,
+            size: "small",
+            color: "dark",
+            countdownFrom: "0",
+            element: "#coinwidget-bitcoin-1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u",
+            onShow: function(){},
+            onHide: function(){}
+          });
+        };
+        script.src = 'https://blockr.io/js_external/coinwidget/coin.js';
+        d.getElementsByTagName('head')[0].appendChild(script);
+      }(document));
   }
 }
 
