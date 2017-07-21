37397b444cbc1cd4a256d82dae89bc663043f8aa
Carles Tubio <ctubio@users.noreply.github.com>
2017-07-21 14:20:57 +0000

    Added broker status out of js.
    Free Software Free Society
    
    To support commits by ctubio,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/lib/gw.h
+++ src/lib/gw.h
@@ -50,11 +50,25 @@ namespace K {
     cout << FN::uiT() << "No gateway provided for exchange \"" << CF::cfString("EXCHANGE") << "\"." << endl;
     exit(1);
   };
+  bool savedQuotingMode = false;
+  bool gwState = false;
+  mConnectivityStatus gwConn = mConnectivityStatus::Disconnected;
+  mConnectivityStatus gwMDConn = mConnectivityStatus::Disconnected;
+  mConnectivityStatus gwEOConn = mConnectivityStatus::Disconnected;
   Gw* gw;
   class GW {
     public:
       static void main(Local<Object> exports) {
         gw = Gw::E(CF::cfExchange());
+        EV::evOn("GatewayMarketConnect", [](Local<Object> c) {
+          gwCon__(mGatewayType::MarketData, (mConnectivityStatus)c->NumberValue());
+        });
+        EV::evOn("GatewayOrderConnect", [](Local<Object> c) {
+          gwCon__(mGatewayType::OrderEntry, (mConnectivityStatus)c->NumberValue());
+        });
+        UI::uiSnap(uiTXT::ExchangeConnectivity, &onSnapStatus);
+        UI::uiSnap(uiTXT::ActiveState, &onSnapState);
+        UI::uiHand(uiTXT::ActiveState, &onHandState);
         NODE_SET_METHOD(exports, "makeFee", GW::_makeFee);
         NODE_SET_METHOD(exports, "takeFee", GW::_takeFee);
         NODE_SET_METHOD(exports, "minTick", GW::_minTick);
@@ -62,8 +76,60 @@ namespace K {
         NODE_SET_METHOD(exports, "exchange", GW::_exchange);
         NODE_SET_METHOD(exports, "setMinTick", GW::_setMinTick);
         NODE_SET_METHOD(exports, "setMinSize", GW::_setMinSize);
+        NODE_SET_METHOD(exports, "setSavedQuotingMode", GW::_setSavedQuotingMode);
       };
     private:
+      static Local<Value> onSnapStatus(Local<Value> z) {
+        Isolate* isolate = Isolate::GetCurrent();
+        Local<Array> k = Array::New(isolate);
+        k->Set(0, Number::New(isolate, (double)gwConn));
+        return k;
+      };
+      static Local<Value> onSnapState(Local<Value> z) {
+        Isolate* isolate = Isolate::GetCurrent();
+        Local<Array> k = Array::New(isolate);
+        k->Set(0, Boolean::New(isolate, gwState));
+        return k;
+      };
+      static Local<Value> onHandState(Local<Value> s) {
+        Isolate* isolate = Isolate::GetCurrent();
+        if (s->BooleanValue() != savedQuotingMode) {
+          savedQuotingMode = s->BooleanValue();
+          gwUpState();
+          Local<Object> o = Object::New(isolate);
+          o->Set(FN::v8S("state"), Boolean::New(isolate, gwState));
+          o->Set(FN::v8S("status"), Number::New(isolate, (double)gwConn));
+          EV::evUp("ExchangeConnect", o);
+        }
+        return (Local<Value>)Undefined(isolate);
+      };
+      static void gwCon__(mGatewayType gwT, mConnectivityStatus gwS) {
+        if (gwT == mGatewayType::MarketData) {
+          if (gwMDConn == gwS) return;
+          gwMDConn = gwS;
+        } else if (gwT == mGatewayType::OrderEntry) {
+          if (gwEOConn == gwS) return;
+          gwEOConn = gwS;
+        }
+        gwConn = gwMDConn == mConnectivityStatus::Connected && gwEOConn == mConnectivityStatus::Connected
+          ? mConnectivityStatus::Connected : mConnectivityStatus::Disconnected;
+        gwUpState();
+        Isolate* isolate = Isolate::GetCurrent();
+        Local<Object> o = Object::New(isolate);
+        o->Set(FN::v8S("state"), Boolean::New(isolate, gwState));
+        o->Set(FN::v8S("status"), Number::New(isolate, (double)gwConn));
+        EV::evUp("ExchangeConnect", o);
+        UI::uiSend(isolate, uiTXT::ExchangeConnectivity, Number::New(isolate, (double)gwConn)->ToObject());
+      };
+      static void gwUpState() {
+        Isolate* isolate = Isolate::GetCurrent();
+        bool newMode = gwConn != mConnectivityStatus::Connected ? false : savedQuotingMode;
+        if (newMode != gwState) {
+          gwState = newMode;
+          cout << FN::uiT() << "GW changed quoting state to " << (gwState ? "Enabled" : "Disabled") << "." << endl;
+          UI::uiSend(isolate, uiTXT::ActiveState, Boolean::New(isolate, gwState)->ToObject());
+        }
+      }
       static void _makeFee(const FunctionCallbackInfo<Value> &args) {
         Isolate* isolate = args.GetIsolate();
         HandleScope scope(isolate);
@@ -94,6 +160,9 @@ namespace K {
       };
       static void _setMinSize(const FunctionCallbackInfo<Value> &args) {
         gw->minSize = args[0]->NumberValue();
+      };
+      static void _setSavedQuotingMode(const FunctionCallbackInfo<Value> &args) {
+        savedQuotingMode = args[0]->BooleanValue();
       };
   };
 }
