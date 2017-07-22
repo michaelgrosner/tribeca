65ce4dcaec1f5e63b3fc0e3d4d4afd9a48ea09ab
Carles Tubio <ctubio@users.noreply.github.com>
2017-07-22 19:44:23 +0000

    Added market trades out of js.
    Free Software Free Society
    
    To support commits by ctubio,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

---
+++ src/lib/mt.h
@@ -0,0 +1,61 @@
+#ifndef K_MT_H_
+#define K_MT_H_
+
+namespace K {
+  struct mGWmt {
+    mExchange exchange;
+    int base;
+    int quote;
+    double price;
+    double size;
+    double time;
+    mSide make_side;
+    mGWmt(mExchange e_, int b_, int q_, double p_, double s_, double t_, mSide m_):
+      exchange(e_), base(b_), quote(q_), price(p_), size(s_), time(t_), make_side(m_) {}
+  };
+  vector<mGWmt> mGWmt_;
+  class MT {
+    public:
+      static void main(Local<Object> exports) {
+        EV::evOn("MarketTradeGateway", [](Local<Object> o) {
+          mGWmt t(
+            gw->exchange(),
+            CF::cfBase(),
+            CF::cfQuote(),
+            o->Get(FN::v8S("price"))->NumberValue(),
+            o->Get(FN::v8S("size"))->NumberValue(),
+            FN::T(),
+            (mSide)o->Get(FN::v8S("make_side"))->NumberValue()
+          );
+          mGWmt_.push_back(t);
+          if (mGWmt_.size()>69) mGWmt_.erase(mGWmt_.begin());
+          EV::evUp("MarketTrade");
+          UI::uiSend(Isolate::GetCurrent(), uiTXT::MarketTrade, v8mGWmt(t));
+        });
+        UI::uiSnap(uiTXT::MarketTrade, &onSnap);
+      };
+    private:
+      static Local<Value> onSnap(Local<Value> z) {
+        Isolate* isolate = Isolate::GetCurrent();
+        Local<Array> k = Array::New(isolate);
+        for (unsigned i=0; i<mGWmt_.size(); ++i) k->Set(i, v8mGWmt(mGWmt_[i]));
+        return k;
+      };
+      static Local<Object> v8mGWmt(mGWmt t) {
+        Isolate* isolate = Isolate::GetCurrent();
+        Local<Object> o = Object::New(isolate);
+        o->Set(FN::v8S("exchange"), Number::New(isolate, (double)t.exchange));
+        Local<Object> k = Object::New(isolate);
+        k->Set(FN::v8S("base"), Number::New(isolate, (double)t.base));
+        k->Set(FN::v8S("quote"), Number::New(isolate, (double)t.quote));
+        o->Set(FN::v8S("pair"), k);
+        o->Set(FN::v8S("price"), Number::New(isolate, t.price));
+        o->Set(FN::v8S("size"), Number::New(isolate, t.size));
+        o->Set(FN::v8S("time"), Number::New(isolate, t.time));
+        o->Set(FN::v8S("make_side"), Number::New(isolate, (double)t.make_side));
+        return o;
+      };
+  };
+}
+
+#endif
