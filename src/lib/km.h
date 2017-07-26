3ac1721019f3e12fc41d6346cb13efb8eb647eb2
Carles Tubio <ctubio@users.noreply.github.com>
2017-07-26 16:54:59 +0000

    Updated source without useless vars.
    Free Software Free Society
    
    To support commits by ctubio,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/lib/km.h
+++ src/lib/km.h
@@ -2,7 +2,6 @@
 #define K_KM_H_
 
 namespace K {
-  Persistent<Object> qpRepo;
   enum class mExchange: unsigned int { Null, HitBtc, OkCoin, Coinbase, Bitfinex, Korbit, Poloniex };
   vector<string> mCurrency = {
     "BTC", "LTC", "EUR", "GBP", "CNY", "CAD", "ETH", "ETC", "BFX", "RRT", "ZEC", "BCN", "DASH", "DOGE",
@@ -62,6 +61,29 @@ namespace K {
     for (unsigned i=0; i<mCurrency.size(); ++i) if (mCurrency[i] == k) return i;
     cout << FN::uiT() << "Errrror: Use of missing \"" << k << "\" currency." << endl; exit(1);
   };
+  Persistent<Object> qpRepo;
+  string cFname;
+  json cfRepo;
+  json pkRepo;
+  sqlite3* db;
+  string dbFpath;
+  int sqlite3_open(string f, sqlite3** db);
+  int sqlite3_exec(sqlite3** db, string q);
+  uWS::Hub hub(0, true);
+  uv_check_t loop;
+  uv_timer_t uiD_;
+  Persistent<Function> noop;
+  typedef Local<Value> (*uiCb)(Local<Value>);
+  struct uiSess { map<string, Persistent<Function>> _cb; map<string, uiCb> cb; map<uiTXT, vector<CopyablePersistentTraits<Object>::CopyablePersistent>> D; int u = 0; };
+  uWS::Group<uWS::SERVER> *uiGroup = hub.createGroup<uWS::SERVER>(uWS::PERMESSAGE_DEFLATE);
+  int iOSR60 = 0;
+  bool uiOPT = true;
+  unsigned long uiMDT = 0;
+  unsigned long uiDDT = 0;
+  string uiNOTE = "";
+  string uiNK64 = "";
+  Persistent<Function> socket_;
+  Persistent<Object> _app_state;
 }
 
 #endif
