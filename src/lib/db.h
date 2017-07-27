9c7a3d0c7e91feb2a18c52e6b9af6dfc8b7a0a59
Carles Tubio <ctubio@users.noreply.github.com>
2017-07-27 22:01:40 +0000

    Added libK back.
    Free Software Free Society
    
    To support commits by ctubio,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/lib/db.h
+++ src/lib/db.h
@@ -6,7 +6,7 @@ namespace K {
     public:
       static void main(Local<Object> exports) {
         dbFpath = string("/data/db/K.").append(to_string((int)CF::cfExchange())).append(".").append(to_string(CF::cfBase())).append(".").append(to_string(CF::cfQuote())).append(".db");
-        if (sqlite3_open(dbFpath.data(), &db)) { cout << FN::uiT() << sqlite3_errmsg(db) << endl; exit(1); }
+        if (sqlite3_open(dbFpath, &db)) { cout << FN::uiT() << sqlite3_errmsg(db) << endl; exit(1); }
         cout << FN::uiT() << "DB " << dbFpath << " loaded OK." << endl;
         NODE_SET_METHOD(exports, "dbLoad", DB::_load);
         NODE_SET_METHOD(exports, "dbInsert", DB::_insert);
