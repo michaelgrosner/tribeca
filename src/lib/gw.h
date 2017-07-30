6eeeee48312a09d1117fccd66c5f40f5fcc254be
Carles Tubio <ctubio@users.noreply.github.com>
2017-07-30 15:32:19 +0000

    Added Bitfinex market data out of js.
    Free Software Free Society
    
    To support commits by ctubio,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/lib/gw.h
+++ src/lib/gw.h
@@ -80,6 +80,7 @@ namespace K {
       };
       static void gwBookUp(mConnectivityStatus k) {
         Isolate* isolate = Isolate::GetCurrent();
+        HandleScope scope(isolate);
         EV::evUp("GatewayMarketConnect", Number::New(isolate, (double)k));
       };
       static void gwLevelUp(mGWbls ls) {
@@ -323,6 +324,10 @@ namespace K {
   };
   class GwBitfinex: public Gw {
     public:
+      bool stillAlive;
+      map<int, string> chan;
+      vector<mGWbl> a_;
+      vector<mGWbl> b_;
       void fetch() {
         exchange = mExchange::Bitfinex;
         symbol = FN::S2l(string(mCurrency[base]).append(mCurrency[quote]));
@@ -334,7 +339,7 @@ namespace K {
         json k = FN::wJet(string(http).append("/pubticker/").append(symbol));
         if (k.find("last_price") != k.end()) {
           string k_ = to_string(stod(k["last_price"].get<string>()) / 10000);
-          unsigned int i = (stod(k["last_price"].get<string>())<0.00001) ? 1 : 0;
+          unsigned int i = stod(k["last_price"].get<string>())<0.00001 ? 1 : 0;
           for (string::iterator it=k_.begin(); it!=k_.end(); ++it)
             if (*it == '0') i++; else if (*it == '.') continue; else break;
           stringstream os(string("1e-").append(to_string(i>8?8:i)));
@@ -352,7 +357,79 @@ namespace K {
         } else for (json::iterator it = k.begin(); it != k.end(); ++it) if ((*it)["type"] == "exchange")
           GW::gwPosUp(stod((*it)["available"].get<string>()), stod((*it)["amount"].get<string>()) - stod((*it)["available"].get<string>()), FN::S2mC((*it)["currency"]));
       };
-      void book() {};
+      void book() {
+        hub.onConnection([&](uWS::WebSocket<uWS::CLIENT> *w, uWS::HttpRequest req) {
+          GW::gwBookUp(mConnectivityStatus::Connected);
+          string m = string("{\"event\":\"subscribe\",\"channel\":\"book\",\"symbol\":\"t").append(FN::S2u(symbol)).append("\",\"prec\":\"P0\",\"freq\":\"F0\"}");
+          w->send(m.data(), m.length(), uWS::OpCode::TEXT);
+          m = string("{\"event\":\"subscribe\",\"channel\":\"trades\",\"symbol\":\"t").append(FN::S2u(symbol)).append("\"}");
+          w->send(m.data(), m.length(), uWS::OpCode::TEXT);
+        });
+        hub.onDisconnection([](uWS::WebSocket<uWS::CLIENT> *w, int code, char *message, size_t length) { GW::gwBookUp(mConnectivityStatus::Disconnected); });
+        hub.onError([](void *user) { cout << FN::uiT() << "GW " << CF::cfString("EXCHANGE") << " WS Error." << endl; });
+        hub.onMessage([&](uWS::WebSocket<uWS::CLIENT> *w, char *message, size_t length, uWS::OpCode opCode) {
+          json k = json::parse(string(message, length));
+          if (k.find("event") != k.end()) {
+            if (k["event"].get<string>() == "pong") stillAlive = true;
+            else if (k["event"].get<string>() == "ping") { string m = string("{\"event\":\"pong\"}"); w->send(m.data(), m.length(), uWS::OpCode::TEXT); }
+            else if (k["event"].get<string>() == "error") cout << FN::uiT() << "GW " << CF::cfString("EXCHANGE") << " WS Error " << k << endl;
+            else if (k["event"].get<string>() == "info") cout << FN::uiT() << "GW " << CF::cfString("EXCHANGE") << " WS Info " << k << endl;
+            else if (k["event"].get<string>() == "subscribed") chan[k["chanId"].get<int>()] = k["channel"].get<string>();
+          } else if (k.is_array()) {
+            if (k["/1"_json_pointer].is_string() && k["/1"_json_pointer].get<string>() == "hb") stillAlive = true;
+            else if (k["/1"_json_pointer].is_string() && k["/1"_json_pointer].get<string>() == "n") cout << FN::uiT() << "GW " << CF::cfString("EXCHANGE") << " WS Notice " << k << endl;
+            else if (k["/0"_json_pointer].is_number() && chan.find(k["/0"_json_pointer].get<int>()) != chan.end()) {
+              if (chan[k["/0"_json_pointer].get<int>()] == "book") {
+                if (k["/1/0"_json_pointer].is_number()) k["/1"_json_pointer] = { k["/1"_json_pointer] };
+                if (k["/1/0"_json_pointer].is_array())
+                  for (json::iterator it = k["/1"_json_pointer].begin(); it != k["/1"_json_pointer].end(); ++it) {
+                    double p = (*it)["/0"_json_pointer].get<double>();
+                    bool c = (*it)["/1"_json_pointer].get<double>() > 0;
+                    bool s = (*it)["/2"_json_pointer].get<double>() > 0;
+                    double a = abs((*it)["/2"_json_pointer].get<double>());
+                    if (c) {
+                      if (s) b_.push_back(mGWbl(p, a));
+                      else a_.push_back(mGWbl(p, a));
+                    } else {
+                      if (s) {
+                        for (vector<mGWbl>::iterator it_ = b_.begin(); it_ != b_.end();)
+                          if ((*it_).price == p) it_ = b_.erase(it_); else ++it_;
+                      } else for (vector<mGWbl>::iterator it_ = a_.begin(); it_ != a_.end();)
+                        if ((*it_).price == p) it_ = a_.erase(it_); else ++it_;
+                    }
+                  }
+                sort(b_.begin(), b_.end(), [](const mGWbl &a__, const mGWbl &b__) { return a__.price*-1 < b__.price*-1; });
+                sort(a_.begin(), a_.end(), [](const mGWbl &a__, const mGWbl &b__) { return a__.price*1 < b__.price*1; });
+                if (b_.size()>21) b_.resize(21, mGWbl(0, 0));
+                if (a_.size()>21) a_.resize(21, mGWbl(0, 0));
+                vector<mGWbl> b__;
+                vector<mGWbl> a__;
+                for (vector<mGWbl>::iterator it = b_.begin(); it != b_.end(); ++it)
+                  if (b__.size() < 13) b__.push_back(*it); else break;
+                for (vector<mGWbl>::iterator it = a_.begin(); it != a_.end(); ++it)
+                  if (a__.size() < 13) a__.push_back(*it); else break;
+                if (a__.size() && b__.size())
+                  GW::gwLevelUp(mGWbls(b__, a__));
+              } else if (chan[k["/0"_json_pointer].get<int>()] == "trades") {
+                if (k["/1"_json_pointer].is_string() && k["/1"_json_pointer].get<string>() == "te")
+                  k["/1"_json_pointer] = { k["/2"_json_pointer] };
+                if (k["/1"_json_pointer].is_array()) {
+                  if (k["/1/0"_json_pointer].is_number())
+                    k["/1"_json_pointer] = { k["/1"_json_pointer] };
+                  if (k["/1/0"_json_pointer].is_array())
+                    for (json::iterator it = k["/1"_json_pointer].begin(); it != k["/1"_json_pointer].end(); ++it)
+                      GW::gwTradeUp(mGWbt(
+                        (*it)["/3"_json_pointer],
+                        abs((*it)["/2"_json_pointer].get<double>()),
+                        (*it)["/2"_json_pointer].get<double>() > 0 ? mSide::Bid : mSide::Ask
+                      ));
+                }
+              }
+            }
+          }
+        });
+        hub.connect(ws, nullptr);
+      };
   };
   class GwKorbit: public Gw {
     public:
