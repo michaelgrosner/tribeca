0fcd55acb781b85545d5c80c37f67bf7db1fae88
Carles Tubio <ctubio@users.noreply.github.com>
2017-08-13 19:03:03 +0000

    Updated cancelAll timer out of js.
    Free Software Free Society
    
    To support commits by ctubio,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/lib/og.h
+++ src/lib/og.h
@@ -5,6 +5,18 @@ namespace K {
   class OG {
     public:
       static void main(Local<Object> exports) {
+        thread([&]() {
+          if (uv_timer_init(uv_default_loop(), &gwCancelAll_)) { cout << FN::uiT() << "Errrror: GW gwCancelAll_ init timer failed." << endl; exit(1); }
+          gwCancelAll_.data = gw;
+          if (uv_timer_start(&gwCancelAll_, [](uv_timer_t *handle) {
+            Gw* gw = (Gw*) handle->data;
+            Isolate* isolate = Isolate::GetCurrent();
+            HandleScope scope(isolate);
+            Local<Object> o = Local<Object>::New(isolate, qpRepo);
+            if (o->Get(FN::v8S("cancelOrdersAuto"))->BooleanValue())
+              gw->cancelAll();
+          }, 0, 300000)) { cout << FN::uiT() << "Errrror: GW gwCancelAll_ start timer failed." << endl; exit(1); }
+        }).detach();
       }
   };
 }
