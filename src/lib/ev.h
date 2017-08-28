921869bbe442cd33203e1cbab31dbe5ac40e7025
Carles Tubio <ctubio@users.noreply.github.com>
2017-08-28 22:53:35 +0000

    Updated source without useless methods.
    Free Software Free Society
    
    To support commits by ctubio,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/lib/ev.h
+++ src/lib/ev.h
@@ -15,7 +15,6 @@ namespace K {
         signal(SIGINT, out);
         Ev *ev = new Ev;
         NODE_SET_METHOD(exports, "evOn", EV::_evOn);
-        NODE_SET_METHOD(exports, "evUp", EV::_evUp);
       }
       static void evOn(string k, evCb cb) {
         ev.cb[k].push_back(cb);
@@ -87,22 +86,6 @@ namespace K {
         Persistent<Function> _cb;
         _cb.Reset(isolate, Local<Function>::Cast(args[1]));
         ev._cb[k].push_back(_cb);
-      };
-      static void _evUp(const FunctionCallbackInfo<Value> &args) {
-        Isolate* isolate = args.GetIsolate();
-        HandleScope scope(isolate);
-        string k = FN::S8v(args[0]->ToString());
-        if (ev._cb.find(k) != ev._cb.end()) {
-          Local<Value> argv[] = {args[1]};
-          for (vector<CopyablePersistentTraits<Function>::CopyablePersistent>::iterator _cb = ev._cb[k].begin(); _cb != ev._cb[k].end(); ++_cb)
-            Local<Function>::New(isolate, *_cb)->Call(isolate->GetCurrentContext()->Global(), 1, argv);
-        }
-        if (ev.cb.find(k) != ev.cb.end()) {
-          JSON Json;
-          json o = args[1]->IsUndefined() ? json{} : json::parse(FN::S8v(Json.Stringify(isolate->GetCurrentContext(), args[1]->ToObject()).ToLocalChecked()));
-          for (vector<evCb>::iterator cb = ev.cb[k].begin(); cb != ev.cb[k].end(); ++cb)
-            (*cb)(o);
-        }
       };
   };
 }
