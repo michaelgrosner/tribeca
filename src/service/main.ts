344ae18c39b2b3fc3a7e56b739170a8e540296d4
Michael Grosner <michaelgrosner@gmail.com>
2017-10-08 14:05:26 -0400

    Publish all orders based off config

--- src/service/main.ts
+++ src/service/main.ts
@@ -260,8 +260,12 @@ const runTradingSystem = async (classes: SimulationClasses) : Promise<void> => {
     
     const exchange = classes.exchange;
 
-    const [initOrders, initTrades, initMktTrades, initMsgs, initParams, initActive, initRfv] = await Promise.all([
-        orderPersister.loadAll(10000, {source: {$gte: Models.OrderSource.OrderTicket}}),
+    const shouldPublishAllOrders = !config.Has("ShowAllOrders") || config.GetBoolean("ShowAllOrders");
+    const ordersFilter = shouldPublishAllOrders ? {} : {source: {$gte: Models.OrderSource.OrderTicket}};
+
+    const [
+        initOrders, initTrades, initMktTrades, initMsgs, initParams, initActive, initRfv] = await Promise.all([
+        orderPersister.loadAll(10000, ordersFilter),
         tradesPersister.loadAll(10000),
         mktTradePersister.loadAll(100),
         messagesPersister.loadAll(50),
@@ -318,7 +322,7 @@ const runTradingSystem = async (classes: SimulationClasses) : Promise<void> => {
     }, "using the following exchange details");
 
     const orderBroker = new Broker.OrderBroker(timeProvider, broker, gateway.oe, orderPersister, tradesPersister, orderStatusPublisher,
-        tradePublisher, submitOrderReceiver, cancelOrderReceiver, cancelAllOrdersReceiver, messages, orderCache, initOrders, initTrades);
+        tradePublisher, submitOrderReceiver, cancelOrderReceiver, cancelAllOrdersReceiver, messages, orderCache, initOrders, initTrades, shouldPublishAllOrders);
     const marketDataBroker = new Broker.MarketDataBroker(timeProvider, gateway.md, marketDataPublisher, marketDataPersister, messages);
     const positionBroker = new Broker.PositionBroker(timeProvider, broker, gateway.pg, positionPublisher, positionPersister, marketDataBroker);
 
