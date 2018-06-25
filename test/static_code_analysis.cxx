e9dc4a777210581f43fc880060af86bd747b29b3
Carles Tubio <ctubio@users.noreply.github.com>
2018-06-25 17:46:46 +0000

    Added static code analysis test (thanks to viva64.com).
    Free Software Free Society
    
    To support commits by ctubio,
    you can buy-me-a-drink with a small git tip at:
      1GitTipgxvKB3zjCLXRcSgDhC9pivkpc7u
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

---
+++ test/static_code_analysis.cxx
@@ -0,0 +1,5 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+
+#include "../src/server/K.cxx"
