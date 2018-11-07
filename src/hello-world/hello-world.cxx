ae31310351d12e91b9a5b582596f324e3586fc06
Carles Tubio <ctubio@users.noreply.github.com>
2018-11-07 13:38:04 +0000

    Updated source without useless comments.
    Free Software Free Society
    
    To support commits by ctubio,
    you can buy-me-a-drink with a small git tip at:
      1GitTipB6sY2YSbkH3BJHepUB7Cjymi9R1
    
    To force me to write source documentation about K,
    you can buy-me-a-drink with a small git tip at:
      1KDocsmbhUEh7JgmYjuMH3nDivzSM6dphP
    
    I promise to drink chocolate milk in the development of the next commit.
    
    To request new features or in case this commit breaks something for you,
    please create a new github issue with all possible details,
    but never share your API Keys!
    
    Signed-off-by: Carles Tubio <ctubio@users.noreply.github.com>

--- src/hello-world/hello-world.cxx
+++ src/hello-world/hello-world.cxx
@@ -1,17 +1,17 @@
 #include "Krypto.ninja.h"
 #include "hello-world.h"
-                                 /*                                 */
-          ;;;;;;;;;;;;;;;;;;;;;;   /*         Wherever you go..     */
-          ;;;;;    ;;;;    ;;;;;     /*       Whatever you do..     */
-          ;;;;;    ;;    ;;;;;;;       /*     I will be right here  */
-          ;;;;;        ;;;;;;;;;         /*    waiting for coins.   */
-          ;;;;;        ;;;;;;;;;/* .-""-._    Whatever it takes..   */
-using namespace    K;    ;;;;;;;/*(  <>  )`-. Or how my OS breaks,  */
-          ;;;;;    ;;;;    ;;;;;/*|`-..-'|  | I will be right here  */
-          ;;;;;    ;;;;    ;;;;;/*|     :|._/  waiting for coins.   */
-          ;;;;;;;;;;;;;    ;;;;;/*`.____;'    Day after day.        */
-          ;;;;;;;;;;;;;;;;;;;;;;/*                               :wq*/
-                              /*                                    */
+
+          ;;;;;;;;;;;;;;;;;;;;;;
+          ;;;;;    ;;;;    ;;;;;
+          ;;;;;    ;;    ;;;;;;;
+          ;;;;;        ;;;;;;;;;
+          ;;;;;        ;;;;;;;;;
+using namespace    K;    ;;;;;;;
+          ;;;;;    ;;;;    ;;;;;
+          ;;;;;    ;;;;    ;;;;;
+          ;;;;;;;;;;;;;    ;;;;;
+          ;;;;;;;;;;;;;;;;;;;;;;
+
 int main(int argc, char** argv) {
   (args = &options)
     ->main(argc, argv)
